5b4649e0fb218e2db32b6a25cda44a170acb0ed0
ddengster <ed.fan@osrfoundation.org>
2020-03-19 17:16:15 +0800

    copyright wrong company

--- joint_trajectory_controller/include/joint_trajectory_controller/tolerances.hpp
+++ joint_trajectory_controller/include/joint_trajectory_controller/tolerances.hpp
@@ -1,4 +1,4 @@
-// Copyright 2020 Open Source Robotics Foundation, Inc.
+// Copyright (C) 2013, PAL Robotics S.L.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
