50c63999886dca464b30c0d6b6499c7198f1924d
Bence Magyar <bence.magyar.robotics@gmail.com>
2020-06-14 16:18:29 +0100

    diff_drive_controller

---
+++ diff_drive_controller/src/odometry.cpp
@@ -0,0 +1,175 @@
+/*********************************************************************
+ * Software License Agreement (BSD License)
+ *
+ *  Copyright (c) 2013, PAL Robotics, S.L.
+ *  All rights reserved.
+ *
+ *  Redistribution and use in source and binary forms, with or without
+ *  modification, are permitted provided that the following conditions
+ *  are met:
+ *
+ *   * Redistributions of source code must retain the above copyright
+ *     notice, this list of conditions and the following disclaimer.
+ *   * Redistributions in binary form must reproduce the above
+ *     copyright notice, this list of conditions and the following
+ *     disclaimer in the documentation and/or other materials provided
+ *     with the distribution.
+ *   * Neither the name of the PAL Robotics nor the names of its
+ *     contributors may be used to endorse or promote products derived
+ *     from this software without specific prior written permission.
+ *
+ *  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ *  "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ *  LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+ *  FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+ *  COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+ *  INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+ *  BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ *  LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ *  CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ *  LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+ *  ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ *  POSSIBILITY OF SUCH DAMAGE.
+ *********************************************************************/
+
+/*
+ * Author: Enrique Fernández
+ */
+
+#include "diff_drive_controller/odometry.hpp"
+
+namespace diff_drive_controller
+{
+Odometry::Odometry(size_t velocity_rolling_window_size)
+: timestamp_(0.0),
+  x_(0.0),
+  y_(0.0),
+  heading_(0.0),
+  linear_(0.0),
+  angular_(0.0),
+  wheel_separation_(0.0),
+  left_wheel_radius_(0.0),
+  right_wheel_radius_(0.0),
+  left_wheel_old_pos_(0.0),
+  right_wheel_old_pos_(0.0),
+  velocity_rolling_window_size_(velocity_rolling_window_size),
+  linear_accumulator_(RollingWindow::window_size = velocity_rolling_window_size),
+  angular_accumulator_(RollingWindow::window_size = velocity_rolling_window_size)
+{
+}
+
+void Odometry::init(const rclcpp::Time & time)
+{
+  // Reset accumulators and timestamp:
+  resetAccumulators();
+  timestamp_ = time;
+}
+
+bool Odometry::update(double left_pos, double right_pos, const rclcpp::Time & time)
+{
+  // We cannot estimate the speed with very small time intervals:
+  const double dt = time.seconds() - timestamp_.seconds();
+  if (dt < 0.0001) {
+    return false;    // Interval too small to integrate with
+  }
+
+  // Get current wheel joint positions:
+  const double left_wheel_cur_pos = left_pos * left_wheel_radius_;
+  const double right_wheel_cur_pos = right_pos * right_wheel_radius_;
+
+  // Estimate velocity of wheels using old and current position:
+  const double left_wheel_est_vel = left_wheel_cur_pos - left_wheel_old_pos_;
+  const double right_wheel_est_vel = right_wheel_cur_pos - right_wheel_old_pos_;
+
+  // Update old position with current:
+  left_wheel_old_pos_ = left_wheel_cur_pos;
+  right_wheel_old_pos_ = right_wheel_cur_pos;
+
+  // Compute linear and angular diff:
+  const double linear = (right_wheel_est_vel + left_wheel_est_vel) * 0.5;
+  // Now there is a bug about scout angular velocity
+  const double angular = (right_wheel_est_vel - left_wheel_est_vel) / wheel_separation_ * 0.43396;
+
+  // Integrate odometry:
+  integrateExact(linear, angular);
+
+  timestamp_ = time;
+
+  // Estimate speeds using a rolling mean to filter them out:
+  linear_accumulator_(linear / dt);
+  angular_accumulator_(angular / dt);
+
+  linear_ = bacc::rolling_mean(linear_accumulator_);
+  angular_ = bacc::rolling_mean(angular_accumulator_);
+
+  return true;
+}
+
+void Odometry::updateOpenLoop(double linear, double angular, const rclcpp::Time & time)
+{
+  /// Save last linear and angular velocity:
+  linear_ = linear;
+  angular_ = angular;
+
+  /// Integrate odometry:
+  const double dt = time.seconds() - timestamp_.seconds();
+  timestamp_ = time;
+  integrateExact(linear * dt, angular * dt);
+}
+
+void Odometry::resetOdometry()
+{
+  x_ = 0.0;
+  y_ = 0.0;
+  heading_ = 0.0;
+}
+
+void Odometry::setWheelParams(
+  double wheel_separation, double left_wheel_radius,
+  double right_wheel_radius)
+{
+  wheel_separation_ = wheel_separation;
+  left_wheel_radius_ = left_wheel_radius;
+  right_wheel_radius_ = right_wheel_radius;
+}
+
+void Odometry::setVelocityRollingWindowSize(size_t velocity_rolling_window_size)
+{
+  velocity_rolling_window_size_ = velocity_rolling_window_size;
+
+  resetAccumulators();
+}
+
+void Odometry::integrateRungeKutta2(double linear, double angular)
+{
+  const double direction = heading_ + angular * 0.5;
+
+  /// Runge-Kutta 2nd order integration:
+  x_ += linear * cos(direction);
+  y_ += linear * sin(direction);
+  heading_ += angular;
+}
+
+void Odometry::integrateExact(double linear, double angular)
+{
+  if (fabs(angular) < 1e-6) {
+    integrateRungeKutta2(linear, angular);
+  } else {
+    /// Exact integration (should solve problems when angular is zero):
+    const double heading_old = heading_;
+    const double r = linear / angular;
+    heading_ += angular;
+    x_ += r * (sin(heading_) - sin(heading_old));
+    y_ += -r * (cos(heading_) - cos(heading_old));
+  }
+}
+
+void Odometry::resetAccumulators()
+{
+  linear_accumulator_ = RollingMeanAccumulator(
+    RollingWindow::window_size = velocity_rolling_window_size_);
+  angular_accumulator_ = RollingMeanAccumulator(
+    RollingWindow::window_size = velocity_rolling_window_size_);
+}
+
+}  // namespace diff_drive_controller
---
+++ diff_drive_controller/test/test_load_diff_drive_controller.cpp
@@ -0,0 +1,40 @@
+// Copyright 2020 PAL Robotics SL.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#include <gtest/gtest.h>
+#include <memory>
+
+#include "controller_manager/controller_manager.hpp"
+#include "test_robot_hardware/test_robot_hardware.hpp"
+#include "rclcpp/utilities.hpp"
+
+TEST(TestLoadDiffDriveController, load_controller)
+{
+  rclcpp::init(0, nullptr);
+
+  std::shared_ptr<test_robot_hardware::TestRobotHardware> robot =
+    std::make_shared<test_robot_hardware::TestRobotHardware>();
+
+  robot->init();
+
+  std::shared_ptr<rclcpp::Executor> executor =
+    std::make_shared<rclcpp::executors::SingleThreadedExecutor>();
+
+  controller_manager::ControllerManager cm(robot, executor, "test_controller_manager");
+
+  ASSERT_NO_THROW(
+    cm.load_controller(
+      "test_diff_drive_controller",
+      "diff_drive_controller/DiffDriveController"));
+}
