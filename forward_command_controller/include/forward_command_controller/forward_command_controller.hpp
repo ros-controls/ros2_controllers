1bed32569db4f408f68e0f72c85d15f5d3e8cb95
Jordan Palacios <jordan.palacios@pal-robotics.com>
2020-08-18 12:20:41 +0200

    Implemented on_configure()
    * Check joints and interface_name params
    * Check requested joints exist in the robot hardware
    * Initialize joint handles
    * Create command topic subscriber

--- forward_command_controller/include/forward_command_controller/forward_command_controller.hpp
+++ forward_command_controller/include/forward_command_controller/forward_command_controller.hpp
@@ -15,23 +15,27 @@
 #ifndef FORWARD_COMMAND_CONTROLLER__FORWARD_COMMAND_CONTROLLER_HPP_
 #define FORWARD_COMMAND_CONTROLLER__FORWARD_COMMAND_CONTROLLER_HPP_
 
+#include <memory>
+#include <vector>
+
 #include "controller_interface/controller_interface.hpp"
 #include "forward_command_controller/visibility_control.h"
+#include "hardware_interface/joint_handle.hpp"
 #include "rclcpp_lifecycle/node_interfaces/lifecycle_node_interface.hpp"
 #include "rclcpp_lifecycle/state.hpp"
+#include "rclcpp/subscription.hpp"
+#include "realtime_tools/realtime_buffer.h"
+#include "std_msgs/msg/float64_multi_array.hpp"
 
 namespace forward_command_controller
 {
-
 class ForwardCommandController : public controller_interface::ControllerInterface
 {
+  using CmdType = std_msgs::msg::Float64MultiArray;
+
 public:
   FORWARD_COMMAND_CONTROLLER_PUBLIC
   ForwardCommandController();
-
-  FORWARD_COMMAND_CONTROLLER_PUBLIC
-  controller_interface::return_type
-  update() override;
 
   FORWARD_COMMAND_CONTROLLER_PUBLIC
   rclcpp_lifecycle::node_interfaces::LifecycleNodeInterface::CallbackReturn
@@ -45,7 +49,14 @@ public:
   rclcpp_lifecycle::node_interfaces::LifecycleNodeInterface::CallbackReturn
   on_deactivate(const rclcpp_lifecycle::State & previous_state) override;
 
+  FORWARD_COMMAND_CONTROLLER_PUBLIC
+  controller_interface::return_type
+  update() override;
+
 protected:
+  std::vector<hardware_interface::JointHandle> joint_handles_;
+  realtime_tools::RealtimeBuffer<std::shared_ptr<CmdType>> rt_command_ptr_;
+  rclcpp::Subscription<CmdType>::SharedPtr joints_command_subscriber_;
 };
 
 }  // namespace forward_command_controller
--- forward_command_controller/src/forward_command_controller.cpp
+++ forward_command_controller/src/forward_command_controller.cpp
@@ -12,35 +12,94 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+#include <algorithm>
+#include <utility>
+
 #include "forward_command_controller/forward_command_controller.hpp"
 
 namespace forward_command_controller
 {
+using CallbackReturn = ForwardCommandController::CallbackReturn;
 
 ForwardCommandController::ForwardCommandController()
-: controller_interface::ControllerInterface()
+: controller_interface::ControllerInterface(),
+  joint_handles_(),
+  rt_command_ptr_(nullptr),
+  joints_command_subscriber_(nullptr)
 {}
 
-rclcpp_lifecycle::node_interfaces::LifecycleNodeInterface::CallbackReturn
-ForwardCommandController::on_configure(const rclcpp_lifecycle::State & /*previous_state*/)
+CallbackReturn ForwardCommandController::on_configure(
+  const rclcpp_lifecycle::State & /*previous_state*/)
 {
-  return rclcpp_lifecycle::node_interfaces::LifecycleNodeInterface::CallbackReturn::SUCCESS;
+  /// @todo add logging messages with reported error cause
+
+  rclcpp::Parameter joints_param, interface_param;
+  if (!lifecycle_node_->get_parameter("joints", joints_param) ||
+    !lifecycle_node_->get_parameter("interface_name", interface_param))
+  {
+    return CallbackReturn::ERROR;
+  }
+
+  auto joint_names = joints_param.as_string_array();
+  if (joint_names.empty()) {
+    return CallbackReturn::ERROR;
+  }
+
+  auto interface_name = interface_param.as_string();
+  if (interface_name.empty()) {
+    return CallbackReturn::ERROR;
+  }
+
+  if (auto rh_ptr = robot_hardware_.lock()) {
+    const auto registered_joints = rh_ptr->get_registered_joint_names();
+
+    // check all requested joints are present
+    for (const auto & joint_name : joint_names) {
+      if (std::find(
+          registered_joints.cbegin(), registered_joints.cend(),
+          joint_name) == registered_joints.cend())
+      {
+        return CallbackReturn::ERROR;
+      }
+    }
+
+    // get joint handles
+    for (const auto & joint_name : joint_names) {
+      hardware_interface::JointHandle joint_handle(joint_name, interface_name);
+      if (rh_ptr->get_joint_handle(joint_handle) ==
+        hardware_interface::hardware_interface_ret_t::ERROR)
+      {
+        return CallbackReturn::ERROR;
+      }
+      joint_handles_.push_back(std::move(joint_handle));
+    }
+  } else {
+    return CallbackReturn::ERROR;
+  }
+
+  joints_command_subscriber_ = lifecycle_node_->create_subscription<CmdType>(
+    "commands", rclcpp::SystemDefaultsQoS(),
+    [this](const CmdType::SharedPtr msg)
+    {
+      rt_command_ptr_.writeFromNonRT(msg);
+    });
+
+  return CallbackReturn::SUCCESS;
 }
 
-rclcpp_lifecycle::node_interfaces::LifecycleNodeInterface::CallbackReturn
-ForwardCommandController::on_activate(const rclcpp_lifecycle::State & /*previous_state*/)
+CallbackReturn ForwardCommandController::on_activate(
+  const rclcpp_lifecycle::State & /*previous_state*/)
 {
-  return rclcpp_lifecycle::node_interfaces::LifecycleNodeInterface::CallbackReturn::SUCCESS;
+  return CallbackReturn::SUCCESS;
 }
 
-rclcpp_lifecycle::node_interfaces::LifecycleNodeInterface::CallbackReturn
-ForwardCommandController::on_deactivate(const rclcpp_lifecycle::State & /*previous_state*/)
+CallbackReturn ForwardCommandController::on_deactivate(
+  const rclcpp_lifecycle::State & /*previous_state*/)
 {
-  return rclcpp_lifecycle::node_interfaces::LifecycleNodeInterface::CallbackReturn::SUCCESS;
+  return CallbackReturn::SUCCESS;
 }
 
-controller_interface::return_type
-ForwardCommandController::update()
+controller_interface::return_type ForwardCommandController::update()
 {
   return controller_interface::return_type::ERROR;
 }
