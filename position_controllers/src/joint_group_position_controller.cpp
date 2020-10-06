15f0c83335d52d10cbfb2e05855d2b1b097002cb
Jordan Palacios <jordan.palacios@pal-robotics.com>
2020-10-06 09:12:01 +0200

    position_controllers (#92)
    * joint position controller initial class version
    * Set logger name
    * joint position controller now presets the interface name parameter
    This parameter is required by the forward_command_controller
    * Added test_load_joint_position_controller
    * Added test_joint_position_controller
    * joint_position_controller documenttion
    * Renaming to joint_group_position_cotroller
    * Default initialize logger
    * Improved error message log

---
+++ position_controllers/src/joint_group_position_controller.cpp
@@ -0,0 +1,52 @@
+// Copyright 2020 PAL Robotics S.L.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#include "position_controllers/joint_group_position_controller.hpp"
+#include "rclcpp/logging.hpp"
+#include "rclcpp/parameter.hpp"
+
+namespace position_controllers
+{
+using CallbackReturn = JointGroupPositionController::CallbackReturn;
+
+JointGroupPositionController::JointGroupPositionController()
+: forward_command_controller::ForwardCommandController()
+{
+  logger_name_ = "joint position controller";
+}
+
+CallbackReturn JointGroupPositionController::on_configure(
+  const rclcpp_lifecycle::State & previous_state)
+{
+  rclcpp::Parameter interface_param;
+  if (!lifecycle_node_->get_parameter("interface_name", interface_param)) {
+    lifecycle_node_->declare_parameter("interface_name", "position_command");
+  } else {
+    if (interface_param.as_string() != "position_command") {
+      RCLCPP_ERROR_STREAM(
+        rclcpp::get_logger(logger_name_),
+        "'interface_name' already set with invalid value '" <<
+          interface_param.as_string() << "'");
+      return CallbackReturn::ERROR;
+    }
+  }
+  return ForwardCommandController::on_configure(previous_state);
+}
+
+}  // namespace position_controllers
+
+#include "pluginlib/class_list_macros.hpp"
+
+PLUGINLIB_EXPORT_CLASS(
+  position_controllers::JointGroupPositionController, controller_interface::ControllerInterface)
