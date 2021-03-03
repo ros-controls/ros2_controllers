4ac02117d3bbf7f6da0beb9e62020d316c90be30
bailaC <subhasdaas@gmail.com>
2021-03-03 10:33:10 +0530

    Adding FTS controller - v1

--- force_torque_sensor_controller/src/force_torque_sensor_controller.cpp
+++ force_torque_sensor_controller/src/force_torque_sensor_controller.cpp
@@ -21,7 +21,8 @@ namespace force_torque_sensor_controller
 {
 
 ForceTorqueSensorController::ForceTorqueSensorController()
-: controller_interface::ControllerInterface()
+: controller_interface::ControllerInterface(),
+  sensor_state_publisher_(nullptr)
 {}
 
 controller_interface::return_type
@@ -34,8 +35,11 @@ ForceTorqueSensorController::init(const std::string & controller_name)
 
   try {
     auto node = get_node();
-    node->declare_parameter<std::vector<std::string>>("joints", {});
-    node->declare_parameter<std::string>("interface_name", "");
+    node->declare_parameter<std::string>("sensor_name", "");
+    node->declare_parameter<std::vector<std::string>>("state_interface_names", {});
+    node->declare_parameter<std::string>("frame_id", "");
+    node->declare_parameter<int>("fx_range", -1);
+    node->declare_parameter<int>("tz_range", -1);
   } catch (const std::exception & e) {
     fprintf(stderr, "Exception thrown during init stage with message: %s \n", e.what());
     return controller_interface::return_type::ERROR;
@@ -47,24 +51,46 @@ ForceTorqueSensorController::init(const std::string & controller_name)
 CallbackReturn ForceTorqueSensorController::on_configure(
   const rclcpp_lifecycle::State & /*previous_state*/)
 {
-  joint_names_ = node_->get_parameter("joints").as_string_array();
-
-  if (joint_names_.empty()) {
-    RCLCPP_ERROR(get_node()->get_logger(), "'joints' parameter was empty");
+  state_interface_names_ = node_->get_parameter("state_interface_names").as_string_array();
+  if (state_interface_names_.empty()) {
+    RCLCPP_ERROR(get_node()->get_logger(), "'state_interface_names' parameter was empty");
     return CallbackReturn::ERROR;
   }
 
-  // Specialized, child controllers set interfaces before calling init function.
-  if (interface_names_.empty()) {
-    interface_names_ = node_->get_parameter("interface_names").as_string_array();
+  sensor_name_ = node_->get_parameter("sensor_name").as_string();
+  if (sensor_name_.empty()) {
+    RCLCPP_ERROR(get_node()->get_logger(), "'sensor_name' parameter was empty");
+    return CallbackReturn::ERROR;
   }
 
-  if (interface_names_.empty()) {
-    RCLCPP_ERROR(get_node()->get_logger(), "'interface_name' parameter was empty");
+  frame_id_ = node_->get_parameter("frame_id").as_string();
+  if (frame_id_.empty()) {
+    RCLCPP_ERROR(get_node()->get_logger(), "'frame_id' parameter was empty");
+    return CallbackReturn::ERROR;
+  }
+
+  fx_range_ = node_->get_parameter("fx_range").as_int();
+  if (fx_range_ == -1) {
+    RCLCPP_ERROR(get_node()->get_logger(), "'fx_range' parameter was empty");
+    return CallbackReturn::ERROR;
+  }
+
+  tz_range_ = node_->get_parameter("tz_range").as_int();
+  if (tz_range_ == -1) {
+    RCLCPP_ERROR(get_node()->get_logger(), "'tz_range' parameter was empty");
     return CallbackReturn::ERROR;
   }
 
   RCLCPP_INFO_STREAM(get_node()->get_logger(), "configure successful");
+
+  try {
+    // register ft sensor data publisher
+    sensor_state_publisher_ = get_node()->create_publisher<geometry_msgs::msg::WrenchStamped>(
+        "force_torque_sensor", rclcpp::SystemDefaultsQoS());
+  } catch (...) {
+    return CallbackReturn::ERROR;
+  }
+
   return CallbackReturn::SUCCESS;
 }
 
@@ -72,13 +98,7 @@ controller_interface::InterfaceConfiguration
 ForceTorqueSensorController::command_interface_configuration() const
 {
   controller_interface::InterfaceConfiguration command_interfaces_config;
-  command_interfaces_config.type = controller_interface::interface_configuration_type::INDIVIDUAL;
-
-  for (const auto & joint : joint_names_) {
-    for (const auto & interface : interface_names_) {
-      command_interfaces_config.names.push_back(joint + "/" + interface);
-    }
-  }
+  command_interfaces_config.type = controller_interface::interface_configuration_type::NONE;
 
   return command_interfaces_config;
 }
@@ -89,33 +109,11 @@ ForceTorqueSensorController::state_interface_configuration() const
   controller_interface::InterfaceConfiguration state_interfaces_config;
   state_interfaces_config.type = controller_interface::interface_configuration_type::INDIVIDUAL;
 
-  for (const auto & joint : joint_names_) {
-    for (const auto & interface : interface_names_) {
-      state_interfaces_config.names.push_back(joint + "/" + interface);
-    }
+  for (const auto & interface : state_interface_names_) {
+    state_interfaces_config.names.push_back(sensor_name_ + "/" + interface);
   }
 
   return state_interfaces_config;
-}
-
-// Fill ordered_interfaces with references to the matching interfaces
-// in the same order as in joint_names
-template<typename T>
-bool get_ordered_interfaces(
-  std::vector<T> & unordered_interfaces, const std::vector<std::string> & joint_names,
-  const std::string & interface_type, std::vector<std::reference_wrapper<T>> & ordered_interfaces)
-{
-  for (const auto & joint_name : joint_names) {
-    for (auto & command_interface : unordered_interfaces) {
-      if ((command_interface.get_name() == joint_name) &&
-        (command_interface.get_interface_name() == interface_type))
-      {
-        ordered_interfaces.push_back(std::ref(command_interface));
-      }
-    }
-  }
-
-  return joint_names.size() == ordered_interfaces.size();
 }
 
 CallbackReturn ForceTorqueSensorController::on_activate(
@@ -132,9 +130,34 @@ CallbackReturn ForceTorqueSensorController::on_deactivate(
 
 controller_interface::return_type ForceTorqueSensorController::update()
 {
-  for (auto index = 0ul; index < command_interfaces_.size(); ++index) {
-    command_interfaces_[index].set_value(state_interfaces_[index].get_value());
+  geometry_msgs::msg::Vector3 f_vec;
+  geometry_msgs::msg::Vector3 t_vec;
+
+  if (state_interfaces_.size() != state_interface_names_.size())
+    return controller_interface::return_type::ERROR;
+
+  for (auto index = 0ul; index < state_interfaces_.size(); ++index) {
+    switch (index) {
+      case 0:
+        f_vec.set__x(state_interfaces_[index].get_value());
+        break;
+      case 1:
+        t_vec.set__z(state_interfaces_[index].get_value());
+        break;
+      default:
+        break;
+    }
   }
+
+  wrench_state_msg_.header.stamp = get_node()->get_clock()->now();
+  wrench_state_msg_.header.frame_id = frame_id_;
+
+  // update wrench state message
+  wrench_state_msg_.wrench.set__force(f_vec);
+  wrench_state_msg_.wrench.set__torque(t_vec);
+
+  // publish
+  sensor_state_publisher_->publish(wrench_state_msg_);
 
   return controller_interface::return_type::SUCCESS;
 }
