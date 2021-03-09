4bc62d22aabca65c5eaa9516b32d6297d2d8e429
Denis Štogl <denis@stogl.de>
2021-03-09 13:37:00 +0100

    First implementation of multi_interface_fwd_controller

--- forward_command_controller/include/forward_command_controller/multi_interface_forward_controller.hpp
+++ forward_command_controller/include/forward_command_controller/multi_interface_forward_controller.hpp
@@ -20,18 +20,14 @@
 #include <string>
 #include <vector>
 
-#include "controller_interface/controller_interface.hpp"
-#include "rclcpp_lifecycle/node_interfaces/lifecycle_node_interface.hpp"
-#include "rclcpp_lifecycle/state.hpp"
-
-
+#include "forward_command_controller/forward_command_controller.hpp"
 #include "forward_command_controller/visibility_control.h"
 
 namespace forward_command_controller
 {
 using CallbackReturn = rclcpp_lifecycle::node_interfaces::LifecycleNodeInterface::CallbackReturn;
 
-class MultiInterfaceForwardController : public controller_interface::ControllerInterface
+class MultiInterfaceForwardController : public forward_command_controller::ForwardCommandController
 {
 public:
   FORWARD_COMMAND_CONTROLLER_PUBLIC
@@ -41,26 +37,10 @@ public:
   controller_interface::return_type init(const std::string & controller_name) override;
 
   FORWARD_COMMAND_CONTROLLER_PUBLIC
-  controller_interface::InterfaceConfiguration command_interface_configuration() const override;
-
-  FORWARD_COMMAND_CONTROLLER_PUBLIC
-  controller_interface::InterfaceConfiguration state_interface_configuration() const override;
-
-  FORWARD_COMMAND_CONTROLLER_PUBLIC
   CallbackReturn on_configure(const rclcpp_lifecycle::State & previous_state) override;
 
-  FORWARD_COMMAND_CONTROLLER_PUBLIC
-  CallbackReturn on_activate(const rclcpp_lifecycle::State & previous_state) override;
-
-  FORWARD_COMMAND_CONTROLLER_PUBLIC
-  CallbackReturn on_deactivate(const rclcpp_lifecycle::State & previous_state) override;
-
-  FORWARD_COMMAND_CONTROLLER_PUBLIC
-  controller_interface::return_type update() override;
-
 protected:
-  std::vector<std::string> joint_names_;
-  std::vector<std::string> interface_names_;
+  std::vector<std::string> joints_interfaces_;
 };
 
 }  // namespace forward_command_controller
