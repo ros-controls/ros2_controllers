de6cb454a49641c2616bb47cbad27d83b977b171
Denis Stogl <denis.stogl@picknik.ai>
2021-03-09 10:37:57 +0100

    forward controller prepared for inhritance

--- forward_command_controller/include/forward_command_controller/forward_command_controller.hpp
+++ forward_command_controller/include/forward_command_controller/forward_command_controller.hpp
@@ -75,6 +75,8 @@ protected:
   std::vector<std::string> joint_names_;
   std::string interface_name_;
 
+  std::vector<std::string> command_interface_types_;
+
   realtime_tools::RealtimeBuffer<std::shared_ptr<CmdType>> rt_command_ptr_;
   rclcpp::Subscription<CmdType>::SharedPtr joints_command_subscriber_;
 
