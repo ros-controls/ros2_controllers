58aedbdcefbc5cef09eb1df60a4475b70be39561
Denis Stogl <denis.stogl@picknik.ai>
2021-03-29 08:51:35 +0200

    Added position and velocity deduction to trajectory.

--- joint_trajectory_controller/src/trajectory.cpp
+++ joint_trajectory_controller/src/trajectory.cpp
@@ -68,7 +68,8 @@ Trajectory::sample(
   const rclcpp::Time & sample_time,
   trajectory_msgs::msg::JointTrajectoryPoint & expected_state,
   TrajectoryPointConstIter & start_segment_itr,
-  TrajectoryPointConstIter & end_segment_itr)
+  TrajectoryPointConstIter & end_segment_itr,
+  bool integrate_missing_states)
 {
   THROW_ON_NULLPTR(trajectory_msg_)
   expected_state = trajectory_msgs::msg::JointTrajectoryPoint();
@@ -93,15 +94,48 @@ Trajectory::sample(
     return false;
   }
 
+  auto add_missing_states = [&](trajectory_msgs::msg::JointTrajectoryPoint & first_state,
+      trajectory_msgs::msg::JointTrajectoryPoint & second_state,
+      const size_t dim, const double delta_t)
+    {
+      if (first_state.velocities.empty()) {
+        first_state.velocities.resize(dim, 0.0);
+      }
+      if (first_state.accelerations.empty()) {
+        first_state.accelerations.resize(dim, 0.0);
+      }
+      if (second_state.positions.empty()) {
+        second_state.positions.resize(dim);
+        if (second_state.velocities.empty()) {
+          second_state.velocities.resize(dim);
+          for (size_t i = 0; i < dim; ++i) {
+            second_state.velocities[i] = first_state.velocities[i] +
+              (first_state.accelerations[i] + second_state.accelerations[i]) * 0.5 * delta_t;
+          }
+        }
+        for (size_t i = 0; i < dim; ++i) {
+          // second state velocity should be reached on the end of the segment, so use middle
+          second_state.positions[i] = first_state.positions[i] +
+            (first_state.velocities[i] + second_state.velocities[i]) * 0.5 * delta_t;
+        }
+      }
+    };
+
   // current time hasn't reached traj time of the first point in the msg yet
-  const auto & first_point_in_msg = trajectory_msg_->points[0];
-  const rclcpp::Duration offset = first_point_in_msg.time_from_start;
-  const rclcpp::Time first_point_timestamp = trajectory_start_time_ + offset;
+  auto & first_point_in_msg = trajectory_msg_->points[0];
+  const rclcpp::Time first_point_timestamp =
+    trajectory_start_time_ + first_point_in_msg.time_from_start;
   if (sample_time < first_point_timestamp) {
-    const rclcpp::Time t0 = time_before_traj_msg_;
+    trajectory_msgs::msg::JointTrajectoryPoint state_before = state_before_traj_msg_;
+    if (integrate_missing_states) {
+      add_missing_states(
+        state_before, first_point_in_msg,
+        state_before_traj_msg_.positions.size(),
+        (first_point_timestamp - time_before_traj_msg_).seconds());
+    }
 
     interpolate_between_points(
-      t0, state_before_traj_msg_, first_point_timestamp, first_point_in_msg,
+      time_before_traj_msg_, state_before, first_point_timestamp, first_point_in_msg,
       sample_time, expected_state);
     start_segment_itr = begin();  // no segments before the first
     end_segment_itr = begin();
@@ -111,13 +145,18 @@ Trajectory::sample(
   // time_from_start + trajectory time is the expected arrival time of trajectory
   const auto last_idx = trajectory_msg_->points.size() - 1;
   for (auto i = 0ul; i < last_idx; ++i) {
-    const auto & point = trajectory_msg_->points[i];
-    const auto & next_point = trajectory_msg_->points[i + 1];
+    auto & point = trajectory_msg_->points[i];
+    auto & next_point = trajectory_msg_->points[i + 1];
 
-    const rclcpp::Duration t0_offset = point.time_from_start;
-    const rclcpp::Duration t1_offset = next_point.time_from_start;
-    const rclcpp::Time t0 = trajectory_start_time_ + t0_offset;
-    const rclcpp::Time t1 = trajectory_start_time_ + t1_offset;
+    const rclcpp::Time t0 = trajectory_start_time_ + point.time_from_start;
+    const rclcpp::Time t1 = trajectory_start_time_ + next_point.time_from_start;
+
+    if (integrate_missing_states) {
+      add_missing_states(
+        point, next_point,
+        state_before_traj_msg_.positions.size(),
+        (t1 - t0).seconds());
+    }
 
     if (sample_time >= t0 && sample_time < t1) {
       interpolate_between_points(t0, point, t1, next_point, sample_time, expected_state);
