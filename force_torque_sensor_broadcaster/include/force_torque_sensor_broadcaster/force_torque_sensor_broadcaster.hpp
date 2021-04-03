c491780cf0f2ab4ce222399ca4480e77ff300bc6
Denis Štogl <denis@stogl.de>
2021-04-03 20:40:49 +0200

    Add some doc. Use FTS semantic components. Correct tests.

--- force_torque_sensor_broadcaster/include/force_torque_sensor_broadcaster/force_torque_sensor_broadcaster.hpp
+++ force_torque_sensor_broadcaster/include/force_torque_sensor_broadcaster/force_torque_sensor_broadcaster.hpp
@@ -26,6 +26,7 @@
 #include "controller_interface/controller_interface.hpp"
 #include "force_torque_sensor_broadcaster/visibility_control.h"
 #include "geometry_msgs/msg/wrench_stamped.hpp"
+#include "semantic_components/force_torque_sensor.hpp"
 #include "rclcpp_lifecycle/node_interfaces/lifecycle_node_interface.hpp"
 #include "rclcpp_lifecycle/state.hpp"
 #include "realtime_tools/realtime_publisher.h"
@@ -71,10 +72,11 @@ protected:
   std::string frame_id_;
   geometry_msgs::msg::WrenchStamped wrench_state_msg_;
 
+  std::unique_ptr<semantic_components::ForceTorqueSensor> force_torque_sensor_;
+
   using StatePublisher = realtime_tools::RealtimePublisher<geometry_msgs::msg::WrenchStamped>;
-  using StatePublisherPtr = std::unique_ptr<StatePublisher>;
   rclcpp::Publisher<geometry_msgs::msg::WrenchStamped>::SharedPtr sensor_state_publisher_;
-  StatePublisherPtr state_publisher_;
+  std::unique_ptr<StatePublisher> realtime_publisher_;
 };
 
 }  // namespace force_torque_sensor_broadcaster
--- force_torque_sensor_broadcaster/test/test_load_force_torque_sensor_broadcaster.cpp
+++ force_torque_sensor_broadcaster/test/test_load_force_torque_sensor_broadcaster.cpp
@@ -37,10 +37,6 @@ TEST(TestLoadForceTorqueSensorBroadcaster, load_controller)
     std::make_unique<hardware_interface::ResourceManager>(
       ros2_control_test_assets::minimal_robot_urdf), executor, "test_controller_manager");
 
-  cm.load_controller(
-    "test_force_torque_sensor_broadcaster",
-    "force_torque_sensor_broadcaster/ForceTorqueSensorBroadcaster");
-
   ASSERT_NO_THROW(
     cm.load_controller(
       "test_force_torque_sensor_broadcaster",
