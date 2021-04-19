bf0aad51011faef2accf18c050b4c38d88727dbb
Denis Štogl <denis@stogl.de>
2021-04-19 17:13:07 +0200

    Add interface for tests

--- force_torque_sensor_broadcaster/test/test_force_torque_sensor_broadcaster.hpp
+++ force_torque_sensor_broadcaster/test/test_force_torque_sensor_broadcaster.hpp
@@ -51,9 +51,19 @@ public:
   void SetUp();
   void TearDown();
 
-  void SetUpStateController();
+  void SetUpFTSBroadcaster();
 
 protected:
+  const std::string sensor_name_ = "fts_sensor";
+  std::array<double, 6> sensor_values_ = {1.1, 2.2, 3.3, 4.4, 5.5, 6.6};
+
+  hardware_interface::StateInterface fts_force_x_{sensor_name_, "force.x", &sensor_values_[0]};
+  hardware_interface::StateInterface fts_force_y_{sensor_name_, "force.y", &sensor_values_[1]};
+  hardware_interface::StateInterface fts_force_z_{sensor_name_, "force.z", &sensor_values_[2]};
+  hardware_interface::StateInterface fts_torque_x_{sensor_name_, "torque.x", &sensor_values_[3]};
+  hardware_interface::StateInterface fts_torque_y_{sensor_name_, "torque.y", &sensor_values_[4]};
+  hardware_interface::StateInterface fts_torque_z_{sensor_name_, "torque.z", &sensor_values_[5]};
+
   std::unique_ptr<FriendForceTorqueSensorBroadcaster> fts_broadcaster_;
 };
 
