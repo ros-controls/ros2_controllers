f3497e31528c65c4809f8c81e99d2afacb202d6b
AndyZe <zelenak@picknik.ai>
2021-04-28 11:28:25 -0500

    Hook up with MoveIt

--- admittance_controller/src/incremental_ik_calculator.cpp
+++ admittance_controller/src/incremental_ik_calculator.cpp
@@ -18,8 +18,14 @@
 
 namespace admittance_controller
 {
-IncrementalIKCalculator::IncrementalIKCalculator()
+IncrementalIKCalculator::IncrementalIKCalculator(std::shared_ptr<rclcpp::Node>& node) : node_(node)
 {
-  ;
+  // TODO(andyz): Parameterize robot description and joint group
+  std::unique_ptr<robot_model_loader::RobotModelLoader> model_loader_ptr =
+      std::unique_ptr<robot_model_loader::RobotModelLoader>(new robot_model_loader::RobotModelLoader(node_, "/robot_description", false /* do not load kinematics plugins */));
+  const moveit::core::RobotModelPtr& kinematic_model = model_loader_ptr->getModel();
+  // TODO(andyz): joint_model_group_ is a raw pointer. Is it thread safe?
+  joint_model_group_ = kinematic_model->getJointModelGroup("gemini");
+  kinematic_state_ = std::make_shared<moveit::core::RobotState>(kinematic_model);
 }
 }  // namespace admittance_controller
