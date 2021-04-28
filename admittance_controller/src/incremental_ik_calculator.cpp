b766b29a29fa8edb4206914306adc3cbd82f9a2d
AndyZe <zelenak@picknik.ai>
2021-04-28 15:19:43 -0500

    Minor renaming to be technically correct

--- admittance_controller/src/incremental_ik_calculator.cpp
+++ admittance_controller/src/incremental_ik_calculator.cpp
@@ -44,18 +44,18 @@ bool IncrementalIKCalculator::convertCartesianDeltasToJointDeltas(Eigen::VectorX
     delta_x_to_jacobian_transform_ = tf_buffer_->lookupTransform(delta_x_frame, moveit_jacobian_frame_, tf2::TimePointZero);
     Eigen::Isometry3d affine_transform = tf2::transformToEigen(delta_x_to_jacobian_transform_);
 
-    // Build the 6x6 adjoint of the transformation matrix
+    // Build the 6x6 transformation matrix
     Eigen::MatrixXd twist_transform(6,6);
     // upper left 3x3 block is the rotation part
     twist_transform.block(0,0,3,3) = affine_transform.rotation();
     // upper right 3x3 block is all zeros
     twist_transform.block(0,4,3,3) = Eigen::MatrixXd::Zero(3,3);
     // lower left 3x3 block is tricky. See https://core.ac.uk/download/pdf/154240607.pdf
-    Eigen::MatrixXd adjoint(3,3);
-    adjoint(0,0) = 0;  adjoint(0,1) = -affine_transform.translation().z();  adjoint(0,2) = affine_transform.translation().y();
-    adjoint(1, 0) = affine_transform.translation().z();  adjoint(1,1) = 0;  adjoint(1,2) = -affine_transform.translation().x();
-    adjoint(2, 0) = -affine_transform.translation().y();  adjoint(2,1) = affine_transform.translation().x();  adjoint(1,2) = 0;
-    twist_transform.block(3,0,3,3) = adjoint * affine_transform.rotation();
+    Eigen::MatrixXd pos_vector_3x3(3,3);
+    pos_vector_3x3(0,0) = 0;  pos_vector_3x3(0,1) = -affine_transform.translation().z();  pos_vector_3x3(0,2) = affine_transform.translation().y();
+    pos_vector_3x3(1, 0) = affine_transform.translation().z();  pos_vector_3x3(1,1) = 0;  pos_vector_3x3(1,2) = -affine_transform.translation().x();
+    pos_vector_3x3(2, 0) = -affine_transform.translation().y();  pos_vector_3x3(2,1) = affine_transform.translation().x();  pos_vector_3x3(1,2) = 0;
+    twist_transform.block(3,0,3,3) = pos_vector_3x3 * affine_transform.rotation();
     // lower right 3x3 block is the rotation part
     twist_transform.block(3,3,3,3) = affine_transform.rotation();
 
