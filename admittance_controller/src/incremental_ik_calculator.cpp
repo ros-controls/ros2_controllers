9511ea0b33a7d61ffc54237fcdb661a4e9aa46b2
AndyZe <zelenak@picknik.ai>
2021-04-28 15:01:46 -0500

    Correct the wrench transformation

--- admittance_controller/src/incremental_ik_calculator.cpp
+++ admittance_controller/src/incremental_ik_calculator.cpp
@@ -38,21 +38,28 @@ IncrementalIKCalculator::IncrementalIKCalculator(std::shared_ptr<rclcpp::Node>& 
 bool IncrementalIKCalculator::convertCartesianDeltasToJointDeltas(Eigen::VectorXd delta_x, std::string& delta_x_frame, Eigen::VectorXd& delta_theta)
 {
   // Transform delta_x to the moveit_jacobian_frame
-  Eigen::Vector3d translation = delta_x.head(3);
-  Eigen::Vector3d rotation = delta_x.tail(3);
   try
   {
-    wrench_to_jacobian_transform_ = tf_buffer_->lookupTransform(delta_x_frame, moveit_jacobian_frame_, tf2::TimePointZero);
+    // 4x4 transformation matrix
+    delta_x_to_jacobian_transform_ = tf_buffer_->lookupTransform(delta_x_frame, moveit_jacobian_frame_, tf2::TimePointZero);
+    Eigen::Isometry3d affine_transform = tf2::transformToEigen(delta_x_to_jacobian_transform_);
 
-    tf2::doTransform(translation, translation, wrench_to_jacobian_transform_);
-    tf2::doTransform(rotation, rotation, wrench_to_jacobian_transform_);
-    // Back to 6x1 vector
-    delta_x[0] = translation[0];
-    delta_x[1] = translation[1];
-    delta_x[2] = translation[2];
-    delta_x[0] = translation[0];
-    delta_x[1] = translation[1];
-    delta_x[2] = translation[2];
+    // Build the 6x6 adjoint of the transformation matrix
+    Eigen::MatrixXd twist_transform(6,6);
+    // upper left 3x3 block is the rotation part
+    twist_transform.block(0,0,3,3) = affine_transform.rotation();
+    // upper right 3x3 block is all zeros
+    twist_transform.block(0,4,3,3) = Eigen::MatrixXd::Zero(3,3);
+    // lower left 3x3 block is tricky. See https://core.ac.uk/download/pdf/154240607.pdf
+    Eigen::MatrixXd adjoint(3,3);
+    adjoint(0,0) = 0;  adjoint(0,1) = -affine_transform.translation().z();  adjoint(0,2) = affine_transform.translation().y();
+    adjoint(1, 0) = affine_transform.translation().z();  adjoint(1,1) = 0;  adjoint(1,2) = -affine_transform.translation().x();
+    adjoint(2, 0) = -affine_transform.translation().y();  adjoint(2,1) = affine_transform.translation().x();  adjoint(1,2) = 0;
+    twist_transform.block(3,0,3,3) = adjoint * affine_transform.rotation();
+    // lower right 3x3 block is the rotation part
+    twist_transform.block(3,3,3,3) = affine_transform.rotation();
+
+    delta_x = twist_transform * delta_x;
   }
   catch (tf2::TransformException & ex)
   {
