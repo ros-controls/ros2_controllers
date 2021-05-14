d110623235744293bd0cd6d1cc0e7dba0c7a798d
AndyZe <zelenak@picknik.ai>
2021-05-14 13:25:17 -0500

    Clean up unused vars and debug statements

--- admittance_controller/include/admittance_controller/incremental_kinematics.hpp
+++ admittance_controller/include/admittance_controller/incremental_kinematics.hpp
@@ -62,8 +62,6 @@ private:
 
   // Pre-allocate for speed
   Eigen::MatrixXd jacobian_;
-  Eigen::JacobiSVD<Eigen::MatrixXd> svd_;
-  Eigen::MatrixXd matrix_s_;
   Eigen::MatrixXd pseudo_inverse_;
 };
 
