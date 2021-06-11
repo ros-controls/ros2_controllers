21854a4e9710f92e983461cd8021aeb7de724e01
Akash Munagala <akash.munagala@gmail.com>
2021-06-10 20:14:15 -0700

    Fix MSVC build for diff_drive_controller test
    Add define for M_PI

--- diff_drive_controller/test/test_accumulator.cpp
+++ diff_drive_controller/test/test_accumulator.cpp
@@ -12,6 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+#define _USE_MATH_DEFINES
 #include <diff_drive_controller/rolling_mean_accumulator.hpp>
 
 #include <gmock/gmock.h>
