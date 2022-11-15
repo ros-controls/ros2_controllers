82e7cf239e23e9d9df9f4ee831eebeb1a50c85df
GiridharBukka <giridhar6937@gmail.com>
2022-11-15 17:47:29 +0100

    added test_time_stamp_zero test

--- ackermann_steering_controller/test/test_ackermann_steering_controller.hpp
+++ ackermann_steering_controller/test/test_ackermann_steering_controller.hpp
@@ -1,4 +1,5 @@
-// Copyright (c) 2022, Stogl Robotics Consulting UG (haftungsbeschränkt) (template)
+// Copyright (c) 2022, Stogl Robotics Consulting UG (haftungsbeschränkt)
+// (template)
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -23,9 +24,8 @@
 #include <utility>
 #include <vector>
 
-#include "gmock/gmock.h"
-
 #include "ackermann_steering_controller/ackermann_steering_controller.hpp"
+#include "gmock/gmock.h"
 #include "hardware_interface/loaned_command_interface.hpp"
 #include "hardware_interface/loaned_state_interface.hpp"
 #include "hardware_interface/types/hardware_interface_return_values.hpp"
@@ -36,15 +36,14 @@
 #include "rclcpp_lifecycle/node_interfaces/lifecycle_node_interface.hpp"
 
 // TODO(anyone): replace the state and command message types
-using ControllerStateMsgOdom =
-  ackermann_steering_controller::AckermannSteeringController::ControllerStateMsgOdom;
-using ControllerStateMsgTf =
-  ackermann_steering_controller::AckermannSteeringController::ControllerStateMsgTf;
-using ControllerReferenceMsg =
-  ackermann_steering_controller::AckermannSteeringController::ControllerReferenceMsg;
+using ControllerStateMsgOdom = ackermann_steering_controller::
+    AckermannSteeringController::ControllerStateMsgOdom;
+using ControllerStateMsgTf = ackermann_steering_controller::
+    AckermannSteeringController::ControllerStateMsgTf;
+using ControllerReferenceMsg = ackermann_steering_controller::
+    AckermannSteeringController::ControllerReferenceMsg;
 
-namespace
-{
+namespace {
 constexpr auto NODE_SUCCESS = controller_interface::CallbackReturn::SUCCESS;
 constexpr auto NODE_ERROR = controller_interface::CallbackReturn::ERROR;
 }  // namespace
@@ -52,112 +51,120 @@ constexpr auto NODE_ERROR = controller_interface::CallbackReturn::ERROR;
 
 // subclassing and friending so we can access member variables
 class TestableAckermannSteeringController
-: public ackermann_steering_controller::AckermannSteeringController
-{
-  FRIEND_TEST(AckermannSteeringControllerTest, all_parameters_set_configure_success);
+    : public ackermann_steering_controller::AckermannSteeringController {
+  FRIEND_TEST(AckermannSteeringControllerTest,
+              all_parameters_set_configure_success);
   FRIEND_TEST(AckermannSteeringControllerTest, check_exported_intefaces);
   FRIEND_TEST(AckermannSteeringControllerTest, activate_success);
   FRIEND_TEST(AckermannSteeringControllerTest, update_success);
   FRIEND_TEST(AckermannSteeringControllerTest, deactivate_success);
   FRIEND_TEST(AckermannSteeringControllerTest, reactivate_success);
   FRIEND_TEST(AckermannSteeringControllerTest, publish_status_success);
-  FRIEND_TEST(AckermannSteeringControllerTest, receive_message_and_publish_updated_status);
+  FRIEND_TEST(AckermannSteeringControllerTest,
+              receive_message_and_publish_updated_status);
   FRIEND_TEST(AckermannSteeringControllerTest, test_message_timeout);
+  FRIEND_TEST(AckermannSteeringControllerTest, test_time_stamp_zero);
   FRIEND_TEST(AckermannSteeringControllerTest, test_message_accepted);
   FRIEND_TEST(AckermannSteeringControllerTest, test_update_logic);
   FRIEND_TEST(AckermannSteeringControllerTest, test_update_logic_chainable);
-  FRIEND_TEST(AckermannSteeringControllerTest, test_ref_timeout_zero_for_update);
-  FRIEND_TEST(AckermannSteeringControllerTest, test_ref_timeout_zero_for_reference_callback);
+  FRIEND_TEST(AckermannSteeringControllerTest,
+              test_ref_timeout_zero_for_update);
+  FRIEND_TEST(AckermannSteeringControllerTest,
+              test_ref_timeout_zero_for_reference_callback);
 
-public:
+ public:
   controller_interface::CallbackReturn on_configure(
-    const rclcpp_lifecycle::State & previous_state) override
-  {
-    auto ret =
-      ackermann_steering_controller::AckermannSteeringController::on_configure(previous_state);
+      const rclcpp_lifecycle::State& previous_state) override {
+    auto ret = ackermann_steering_controller::AckermannSteeringController::
+        on_configure(previous_state);
     // Only if on_configure is successful create subscription
-    if (ret == CallbackReturn::SUCCESS)
-    {
+    if (ret == CallbackReturn::SUCCESS) {
       ref_subscriber_wait_set_.add_subscription(ref_subscriber_);
     }
     return ret;
   }
 
   controller_interface::CallbackReturn on_activate(
-    const rclcpp_lifecycle::State & previous_state) override
-  {
+      const rclcpp_lifecycle::State& previous_state) override {
     auto ref_itfs = on_export_reference_interfaces();
-    return ackermann_steering_controller::AckermannSteeringController::on_activate(previous_state);
+    return ackermann_steering_controller::AckermannSteeringController::
+        on_activate(previous_state);
   }
 
   /**
-   * @brief wait_for_command blocks until a new ControllerReferenceMsg is received.
-   * Requires that the executor is not spinned elsewhere between the
+   * @brief wait_for_command blocks until a new ControllerReferenceMsg is
+   * received. Requires that the executor is not spinned elsewhere between the
    *  message publication and the call to this function.
    *
-   * @return true if new ControllerReferenceMsg msg was received, false if timeout.
+   * @return true if new ControllerReferenceMsg msg was received, false if
+   * timeout.
    */
-  bool wait_for_command(
-    rclcpp::Executor & executor, rclcpp::WaitSet & subscriber_wait_set,
-    const std::chrono::milliseconds & timeout = std::chrono::milliseconds{500})
-  {
-    bool success = subscriber_wait_set.wait(timeout).kind() == rclcpp::WaitResultKind::Ready;
-    if (success)
-    {
+  bool wait_for_command(rclcpp::Executor& executor,
+                        rclcpp::WaitSet& subscriber_wait_set,
+                        const std::chrono::milliseconds& timeout =
+                            std::chrono::milliseconds{500}) {
+    bool success = subscriber_wait_set.wait(timeout).kind() ==
+                   rclcpp::WaitResultKind::Ready;
+    if (success) {
       executor.spin_some();
     }
     return success;
   }
 
-  bool wait_for_commands(
-    rclcpp::Executor & executor,
-    const std::chrono::milliseconds & timeout = std::chrono::milliseconds{500})
-  {
+  bool wait_for_commands(rclcpp::Executor& executor,
+                         const std::chrono::milliseconds& timeout =
+                             std::chrono::milliseconds{500}) {
     return wait_for_command(executor, ref_subscriber_wait_set_, timeout);
   }
 
-  // TODO(anyone): add implementation of any methods of your controller is needed
+  // TODO(anyone): add implementation of any methods of your controller is
+  // needed
 
-private:
+ private:
   rclcpp::WaitSet ref_subscriber_wait_set_;
 };
 
-// We are using template class here for easier reuse of Fixture in specializations of controllers
+// We are using template class here for easier reuse of Fixture in
+// specializations of controllers
 template <typename CtrlType>
-class AckermannSteeringControllerFixture : public ::testing::Test
-{
-public:
+class AckermannSteeringControllerFixture : public ::testing::Test {
+ public:
   static void SetUpTestCase() {}
 
-  void SetUp()
-  {
+  void SetUp() {
     // initialize controller
     controller_ = std::make_unique<CtrlType>();
 
-    command_publisher_node_ = std::make_shared<rclcpp::Node>("command_publisher");
-    command_publisher_ = command_publisher_node_->create_publisher<ControllerReferenceMsg>(
-      "/test_ackermann_steering_controller/reference", rclcpp::SystemDefaultsQoS());
+    command_publisher_node_ =
+        std::make_shared<rclcpp::Node>("command_publisher");
+    command_publisher_ =
+        command_publisher_node_->create_publisher<ControllerReferenceMsg>(
+            "/test_ackermann_steering_controller/reference",
+            rclcpp::SystemDefaultsQoS());
   }
 
   static void TearDownTestCase() {}
 
   void TearDown() { controller_.reset(nullptr); }
 
-protected:
-  void SetUpController(const std::string controller_name = "test_ackermann_steering_controller")
-  {
-    ASSERT_EQ(controller_->init(controller_name), controller_interface::return_type::OK);
+ protected:
+  void SetUpController(const std::string controller_name =
+                           "test_ackermann_steering_controller") {
+    ASSERT_EQ(controller_->init(controller_name),
+              controller_interface::return_type::OK);
 
     std::vector<hardware_interface::LoanedCommandInterface> command_ifs;
     command_itfs_.reserve(joint_command_values_.size());
     command_ifs.reserve(joint_command_values_.size());
 
     command_itfs_.emplace_back(hardware_interface::CommandInterface(
-      rear_wheel_name, hardware_interface::HW_IF_VELOCITY, &joint_command_values_[0]));
+        rear_wheel_name, hardware_interface::HW_IF_VELOCITY,
+        &joint_command_values_[0]));
     command_ifs.emplace_back(command_itfs_.back());
 
     command_itfs_.emplace_back(hardware_interface::CommandInterface(
-      front_steer_name, hardware_interface::HW_IF_VELOCITY, &joint_command_values_[1]));
+        front_steer_name, hardware_interface::HW_IF_VELOCITY,
+        &joint_command_values_[1]));
     command_ifs.emplace_back(command_itfs_.back());
     // TODO(anyone): Add other command interfaces, if any
 
@@ -166,24 +173,28 @@ protected:
     state_ifs.reserve(joint_state_values_.size());
 
     state_itfs_.emplace_back(hardware_interface::StateInterface(
-      rear_wheel_name, hardware_interface::HW_IF_POSITION, &joint_state_values_[0]));
+        rear_wheel_name, hardware_interface::HW_IF_POSITION,
+        &joint_state_values_[0]));
     state_ifs.emplace_back(state_itfs_.back());
 
     state_itfs_.emplace_back(hardware_interface::StateInterface(
-      front_steer_name, hardware_interface::HW_IF_POSITION, &joint_state_values_[1]));
+        front_steer_name, hardware_interface::HW_IF_POSITION,
+        &joint_state_values_[1]));
     state_ifs.emplace_back(state_itfs_.back());
     // TODO(anyone): Add other state interfaces, if any
 
-    controller_->assign_interfaces(std::move(command_ifs), std::move(state_ifs));
+    controller_->assign_interfaces(std::move(command_ifs),
+                                   std::move(state_ifs));
   }
 
-  void subscribe_and_get_messages(ControllerStateMsgOdom & msg_odom)
-  {
+  void subscribe_and_get_messages(ControllerStateMsgOdom& msg_odom) {
     // create a new subscriber
     rclcpp::Node test_subscription_node("test_subscription_node");
     auto subs_callback_odom = [&](const ControllerStateMsgOdom::SharedPtr) {};
-    auto subscription_odom = test_subscription_node.create_subscription<ControllerStateMsgOdom>(
-      "/test_ackermann_steering_controller/odometry", 10, subs_callback_odom);
+    auto subscription_odom =
+        test_subscription_node.create_subscription<ControllerStateMsgOdom>(
+            "/test_ackermann_steering_controller/odometry", 10,
+            subs_callback_odom);
 
     // call update to publish the test value
     ASSERT_EQ(
@@ -194,7 +205,8 @@ protected:
       controller_interface::return_type::OK);
 
     // call update to publish the test value
-    // since update doesn't guarantee a published message, republish until received
+    // since update doesn't guarantee a published message, republish until
+    // received
     int max_sub_check_loop_count = 5;  // max number of tries for pub/sub loop
     rclcpp::WaitSet wait_set;          // block used to wait on message
     wait_set.add_subscription(subscription_odom);
@@ -202,16 +214,17 @@ protected:
     {
       controller_->update_reference_from_subscribers();
       controller_->update_and_write_commands(
-        controller_->get_node()->now(), rclcpp::Duration::from_seconds(0.01));
+          controller_->get_node()->now(), rclcpp::Duration::from_seconds(0.01));
       // check if message has been received
-      if (wait_set.wait(std::chrono::milliseconds(2)).kind() == rclcpp::WaitResultKind::Ready)
-      {
+      if (wait_set.wait(std::chrono::milliseconds(2)).kind() ==
+          rclcpp::WaitResultKind::Ready) {
         break;
       }
     }
 
-    ASSERT_GE(max_sub_check_loop_count, 0) << "Test was unable to publish a message through "
-                                              "controller/broadcaster update loop";
+    ASSERT_GE(max_sub_check_loop_count, 0)
+        << "Test was unable to publish a message through "
+           "controller/broadcaster update loop";
 
     // take message from subscription
     rclcpp::MessageInfo msg_odom_info;
@@ -219,19 +232,15 @@ protected:
   }
 
   // TODO(anyone): add/remove arguments as it suites your command message type
-  void publish_commands(
-    const rclcpp::Time & stamp, const double & twist_linear_x = 0.45,
-    const double & twist_angular_z = 0.0)
-  {
-    auto wait_for_topic = [&](const auto topic_name)
-    {
+  void publish_commands(const rclcpp::Time& stamp,
+                        const double& twist_linear_x = 0.45,
+                        const double& twist_angular_z = 0.0) {
+    auto wait_for_topic = [&](const auto topic_name) {
       size_t wait_count = 0;
-      while (command_publisher_node_->count_subscribers(topic_name) == 0)
-      {
-        if (wait_count >= 5)
-        {
-          auto error_msg =
-            std::string("publishing to ") + topic_name + " but no node subscribes to it";
+      while (command_publisher_node_->count_subscribers(topic_name) == 0) {
+        if (wait_count >= 5) {
+          auto error_msg = std::string("publishing to ") + topic_name +
+                           " but no node subscribes to it";
           throw std::runtime_error(error_msg);
         }
         std::this_thread::sleep_for(std::chrono::milliseconds(100));
@@ -254,7 +263,7 @@ protected:
     command_publisher_->publish(msg);
   }
 
-protected:
+ protected:
   // TODO(anyone): adjust the members as needed
   std::string rear_wheel_name = "rear_wheel_joint";
   std::string front_steer_name = "front_steer_joint";
