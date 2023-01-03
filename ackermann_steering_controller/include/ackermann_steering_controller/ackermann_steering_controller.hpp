378d88b364d7cdb2e4a62c1089a9359f16ee4a16
Denis Štogl <denis@stoglrobotics.de>
2023-01-03 19:35:35 +0100

    Move Ackermann steering controller to dedicated package.

---
+++ ackermann_steering_controller/include/ackermann_steering_controller/ackermann_steering_controller.hpp
@@ -0,0 +1,58 @@
+// Copyright (c) 2022, Stogl Robotics Consulting UG (haftungsbeschränkt)
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+// Authors: dr. sc. Tomislav Petkovic, Dr. Ing. Denis Štogl
+//
+
+#ifndef ACKERMANN_STEERING_CONTROLLER__ACKERMANN_STEERING_CONTROLLER_HPP_
+#define ACKERMANN_STEERING_CONTROLLER__ACKERMANN_STEERING_CONTROLLER_HPP_
+
+#include <memory>
+
+#include "ackermann_steering_controller/visibility_control.h"
+#include "ackermann_steering_controller_parameters.hpp"
+#include "steering_controllers_library/steering_controllers_library.hpp"
+
+namespace ackermann_steering_controller
+{
+// name constants for state interfaces
+static constexpr size_t STATE_DRIVE_RIGHT_WHEEL = 0;
+static constexpr size_t STATE_DRIVE_LEFT_WHEEL = 1;
+static constexpr size_t STATE_STEER_RIGHT_WHEEL = 2;
+static constexpr size_t STATE_STEER_LEFT_WHEEL = 4;
+
+// name constants for command interfaces
+static constexpr size_t CMD_DRIVE_WHEEL = 0;
+static constexpr size_t CMD_STEER = 1;
+
+class AckermannSteeringController : public steering_controllers_library::SteeringControllersLibrary
+{
+public:
+  AckermannSteeringController();
+
+  ACKERMANN_STEERING_CONTROLLER__VISIBILITY_PUBLIC controller_interface::CallbackReturn
+  configure_odometry() override;
+
+  ACKERMANN_STEERING_CONTROLLER__VISIBILITY_PUBLIC bool update_odometry(
+    const rclcpp::Duration & period) override;
+
+  ACKERMANN_STEERING_CONTROLLER__VISIBILITY_PUBLIC void
+  initialize_implementation_parameter_listener() override;
+
+private:
+  std::shared_ptr<ackermann_steering_controller::ParamListener> ackermann_param_listener_;
+};
+}  // namespace ackermann_steering_controller
+
+#endif  // ACKERMANN_STEERING_CONTROLLER__ACKERMANN_STEERING_CONTROLLER_HPP_
---
+++ ackermann_steering_controller/src/ackermann_steering_controller.cpp
@@ -0,0 +1,102 @@
+// Copyright (c) 2022, Stogl Robotics Consulting UG (haftungsbeschränkt)
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#include "ackermann_steering_controller/ackermann_steering_controller.hpp"
+
+namespace ackermann_steering_controller
+{
+AckermannSteeringController::AckermannSteeringController()
+: steering_controllers_library::SteeringControllersLibrary()
+{
+}
+
+void AckermannSteeringController::initialize_implementation_parameter_listener()
+{
+  ackermann_param_listener_ =
+    std::make_shared<ackermann_steering_controller::ParamListener>(get_node());
+}
+
+controller_interface::CallbackReturn AckermannSteeringController::configure_odometry()
+{
+  ackermann_steering_controller::Params ackerman_params = ackermann_param_listener_->get_params();
+
+  const double front_wheels_radius = ackerman_params.front_wheels_radius;
+  const double rear_wheels_radius = ackerman_params.rear_wheels_radius;
+  const double front_wheel_track = ackerman_params.front_wheel_track;
+  const double rear_wheel_track = ackerman_params.rear_wheel_track;
+  const double wheelbase = ackerman_params.wheelbase;
+
+  if (params_.front_steering)
+  {
+    odometry_.set_wheel_params(rear_wheels_radius, wheelbase, rear_wheel_track);
+  }
+  else
+  {
+    odometry_.set_wheel_params(front_wheels_radius, wheelbase, front_wheel_track);
+  }
+
+  odometry_.set_odometry_type(steering_odometry::ACKERMANN_CONFIG);
+
+  const size_t nr_state_itfs = 4;
+  const size_t nr_cmd_itfs = 4;
+  const size_t nr_ref_itfs = 2;
+
+  set_interface_numbers(nr_state_itfs, nr_cmd_itfs, nr_ref_itfs);
+
+  RCLCPP_INFO(get_node()->get_logger(), "ackermann odom configure successful");
+  return controller_interface::CallbackReturn::SUCCESS;
+}
+
+bool AckermannSteeringController::update_odometry(const rclcpp::Duration & period)
+{
+  if (params_.open_loop)
+  {
+    odometry_.update_open_loop(last_linear_velocity_, last_angular_velocity_, period.seconds());
+  }
+  else
+  {
+    const double rear_right_wheel_value = state_interfaces_[STATE_DRIVE_RIGHT_WHEEL].get_value();
+    const double rear_left_wheel_value = state_interfaces_[STATE_DRIVE_LEFT_WHEEL].get_value();
+    const double front_right_steer_position =
+      state_interfaces_[STATE_STEER_RIGHT_WHEEL].get_value();
+    const double front_left_steer_position = state_interfaces_[STATE_STEER_LEFT_WHEEL].get_value();
+    if (
+      !std::isnan(rear_right_wheel_value) && !std::isnan(rear_left_wheel_value) &&
+      !std::isnan(front_right_steer_position) && !std::isnan(front_left_steer_position))
+    {
+      if (params_.position_feedback)
+      {
+        // Estimate linear and angular velocity using joint information
+        odometry_.update_from_position(
+          rear_right_wheel_value, rear_left_wheel_value, front_right_steer_position,
+          front_left_steer_position, period.seconds());
+      }
+      else
+      {
+        // Estimate linear and angular velocity using joint information
+        odometry_.update_from_velocity(
+          rear_right_wheel_value, rear_left_wheel_value, front_right_steer_position,
+          front_left_steer_position, period.seconds());
+      }
+    }
+  }
+  return true;
+}
+}  // namespace ackermann_steering_controller
+
+#include "pluginlib/class_list_macros.hpp"
+
+PLUGINLIB_EXPORT_CLASS(
+  ackermann_steering_controller::AckermannSteeringController,
+  controller_interface::ChainableControllerInterface)
