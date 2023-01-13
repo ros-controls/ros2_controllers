c6b51a04d66ae1aa9323fee69db0a95ee3ca27ca
petkovich <tomislav.petkovic.fer@gmail.com>
2023-01-13 17:12:17 +0100

    tri: activate, update, deactivate, reactivate

--- tricycle_steering_controller/test/test_tricycle_steering_controller.cpp
+++ tricycle_steering_controller/test/test_tricycle_steering_controller.cpp
@@ -88,134 +88,59 @@ TEST_F(TricycleSteeringControllerTest, check_exported_intefaces)
   }
 }
 
-// TEST_F(TricycleSteeringControllerTest, activate_success)
-// {
-//   SetUpController();
+TEST_F(TricycleSteeringControllerTest, activate_success)
+{
+  SetUpController();
 
-//   ASSERT_EQ(controller_->on_configure(rclcpp_lifecycle::State()), NODE_SUCCESS);
-//   ASSERT_EQ(controller_->on_activate(rclcpp_lifecycle::State()), NODE_SUCCESS);
+  ASSERT_EQ(controller_->on_configure(rclcpp_lifecycle::State()), NODE_SUCCESS);
+  ASSERT_EQ(controller_->on_activate(rclcpp_lifecycle::State()), NODE_SUCCESS);
 
-//   // check that the message is reset
-//   auto msg = controller_->input_ref_.readFromNonRT();
-//   EXPECT_EQ((*msg)->displacements.size(), joint_names_.size());
-//   for (const auto & cmd : (*msg)->displacements)
-//   {
-//     EXPECT_TRUE(std::isnan(cmd));
-//   }
-//   EXPECT_EQ((*msg)->velocities.size(), joint_names_.size());
-//   for (const auto & cmd : (*msg)->velocities)
-//   {
-//     EXPECT_TRUE(std::isnan(cmd));
-//   }
+  // check that the message is reset
+  auto msg = controller_->input_ref_.readFromNonRT();
+  EXPECT_TRUE(std::isnan((*msg)->twist.linear.x));
+  EXPECT_TRUE(std::isnan((*msg)->twist.linear.y));
+  EXPECT_TRUE(std::isnan((*msg)->twist.linear.z));
+  EXPECT_TRUE(std::isnan((*msg)->twist.angular.x));
+  EXPECT_TRUE(std::isnan((*msg)->twist.angular.y));
+  EXPECT_TRUE(std::isnan((*msg)->twist.angular.z));
+}
 
-//   ASSERT_TRUE(std::isnan((*msg)->duration));
+TEST_F(TricycleSteeringControllerTest, update_success)
+{
+  SetUpController();
 
-//   EXPECT_EQ(controller_->reference_interfaces_.size(), joint_names_.size());
-//   for (const auto & interface : controller_->reference_interfaces_)
-//   {
-//     EXPECT_TRUE(std::isnan(interface));
-//   }
-// }
+  ASSERT_EQ(controller_->on_configure(rclcpp_lifecycle::State()), NODE_SUCCESS);
+  ASSERT_EQ(controller_->on_activate(rclcpp_lifecycle::State()), NODE_SUCCESS);
 
-// TEST_F(TricycleSteeringControllerTest, update_success)
-// {
-//   SetUpController();
+  ASSERT_EQ(
+    controller_->update(rclcpp::Time(0), rclcpp::Duration::from_seconds(0.01)),
+    controller_interface::return_type::OK);
+}
 
-//   ASSERT_EQ(controller_->on_configure(rclcpp_lifecycle::State()), NODE_SUCCESS);
-//   ASSERT_EQ(controller_->on_activate(rclcpp_lifecycle::State()), NODE_SUCCESS);
+TEST_F(TricycleSteeringControllerTest, deactivate_success)
+{
+  SetUpController();
 
-//   ASSERT_EQ(
-//     controller_->update(rclcpp::Time(0), rclcpp::Duration::from_seconds(0.01)),
-//     controller_interface::return_type::OK);
-// }
+  ASSERT_EQ(controller_->on_configure(rclcpp_lifecycle::State()), NODE_SUCCESS);
+  ASSERT_EQ(controller_->on_activate(rclcpp_lifecycle::State()), NODE_SUCCESS);
+  ASSERT_EQ(controller_->on_deactivate(rclcpp_lifecycle::State()), NODE_SUCCESS);
+}
 
-// TEST_F(TricycleSteeringControllerTest, deactivate_success)
-// {
-//   SetUpController();
+TEST_F(TricycleSteeringControllerTest, reactivate_success)
+{
+  SetUpController();
 
-//   ASSERT_EQ(controller_->on_configure(rclcpp_lifecycle::State()), NODE_SUCCESS);
-//   ASSERT_EQ(controller_->on_activate(rclcpp_lifecycle::State()), NODE_SUCCESS);
-//   ASSERT_EQ(controller_->on_deactivate(rclcpp_lifecycle::State()), NODE_SUCCESS);
-// }
+  ASSERT_EQ(controller_->on_configure(rclcpp_lifecycle::State()), NODE_SUCCESS);
+  ASSERT_EQ(controller_->on_activate(rclcpp_lifecycle::State()), NODE_SUCCESS);
+  ASSERT_EQ(controller_->on_deactivate(rclcpp_lifecycle::State()), NODE_SUCCESS);
+  ASSERT_TRUE(std::isnan(controller_->command_interfaces_[0].get_value()));
+  ASSERT_EQ(controller_->on_activate(rclcpp_lifecycle::State()), NODE_SUCCESS);
+  ASSERT_TRUE(std::isnan(controller_->command_interfaces_[0].get_value()));
 
-// TEST_F(TricycleSteeringControllerTest, reactivate_success)
-// {
-//   SetUpController();
-
-//   ASSERT_EQ(controller_->on_configure(rclcpp_lifecycle::State()), NODE_SUCCESS);
-//   ASSERT_EQ(controller_->on_activate(rclcpp_lifecycle::State()), NODE_SUCCESS);
-//   ASSERT_EQ(controller_->command_interfaces_[CMD_STEER].get_value(), 101.101);
-//   ASSERT_EQ(controller_->on_deactivate(rclcpp_lifecycle::State()), NODE_SUCCESS);
-//   ASSERT_TRUE(std::isnan(controller_->command_interfaces_[CMD_STEER].get_value()));
-//   ASSERT_EQ(controller_->on_activate(rclcpp_lifecycle::State()), NODE_SUCCESS);
-//   ASSERT_TRUE(std::isnan(controller_->command_interfaces_[CMD_STEER].get_value()));
-
-//   ASSERT_EQ(
-//     controller_->update(rclcpp::Time(0), rclcpp::Duration::from_seconds(0.01)),
-//     controller_interface::return_type::OK);
-// }
-
-// TEST_F(TricycleSteeringControllerTest, test_setting_slow_mode_service)
-// {
-//   SetUpController();
-
-//   rclcpp::executors::MultiThreadedExecutor executor;
-//   executor.add_node(controller_->get_node()->get_node_base_interface());
-//   executor.add_node(service_caller_node_->get_node_base_interface());
-
-//   // initially set to false
-//   ASSERT_EQ(*(controller_->control_mode_.readFromRT()), control_mode_type::FAST);
-
-//   ASSERT_EQ(controller_->on_configure(rclcpp_lifecycle::State()), NODE_SUCCESS);
-//   ASSERT_EQ(controller_->on_activate(rclcpp_lifecycle::State()), NODE_SUCCESS);
-
-//   // should stay false
-//   ASSERT_EQ(*(controller_->control_mode_.readFromRT()), control_mode_type::FAST);
-
-//   // set to true
-//   ASSERT_NO_THROW(call_service(true, executor));
-//   ASSERT_EQ(*(controller_->control_mode_.readFromRT()), control_mode_type::SLOW);
-
-//   // set back to false
-//   ASSERT_NO_THROW(call_service(false, executor));
-//   ASSERT_EQ(*(controller_->control_mode_.readFromRT()), control_mode_type::FAST);
-// }
-
-// TEST_F(TricycleSteeringControllerTest, test_update_logic_fast)
-// {
-//   SetUpController();
-//   rclcpp::executors::MultiThreadedExecutor executor;
-//   executor.add_node(controller_->get_node()->get_node_base_interface());
-//   executor.add_node(service_caller_node_->get_node_base_interface());
-
-//   ASSERT_EQ(controller_->on_configure(rclcpp_lifecycle::State()), NODE_SUCCESS);
-//   controller_->set_chained_mode(false);
-//   ASSERT_EQ(controller_->on_activate(rclcpp_lifecycle::State()), NODE_SUCCESS);
-//   ASSERT_FALSE(controller_->is_in_chained_mode());
-
-//   // set command statically
-//   static constexpr double TEST_DISPLACEMENT = 23.24;
-//   std::shared_ptr<ControllerReferenceMsg> msg = std::make_shared<ControllerReferenceMsg>();
-//   msg->joint_names = joint_names_;
-//   msg->displacements.resize(joint_names_.size(), TEST_DISPLACEMENT);
-//   msg->velocities.resize(joint_names_.size(), std::numeric_limits<double>::quiet_NaN());
-//   msg->duration = std::numeric_limits<double>::quiet_NaN();
-//   controller_->input_ref_.writeFromNonRT(msg);
-
-//   ASSERT_EQ(
-//     controller_->update(rclcpp::Time(0), rclcpp::Duration::from_seconds(0.01)),
-//     controller_interface::return_type::OK);
-
-//   EXPECT_EQ(*(controller_->control_mode_.readFromRT()), control_mode_type::FAST);
-//   EXPECT_EQ(joint_command_values_[STATE_DRIVE_RIGHT_WHEEL], TEST_DISPLACEMENT);
-//   EXPECT_TRUE(std::isnan((*(controller_->input_ref_.readFromRT()))->displacements[0]));
-//   EXPECT_EQ(*(controller_->control_mode_.readFromRT()), control_mode_type::FAST);
-//   EXPECT_EQ(controller_->reference_interfaces_.size(), joint_names_.size());
-//   for (const auto & interface : controller_->reference_interfaces_)
-//   {
-//     EXPECT_TRUE(std::isnan(interface));
-//   }
-// }
+  ASSERT_EQ(
+    controller_->update(rclcpp::Time(0), rclcpp::Duration::from_seconds(0.01)),
+    controller_interface::return_type::OK);
+}
 
 // TEST_F(TricycleSteeringControllerTest, test_update_logic_slow)
 // {
