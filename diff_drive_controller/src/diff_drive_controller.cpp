9853bc38d903f990bf5c65875a3d4e921df8ab40
Noel Jimenez <noel.jimenez.gar@gmail.com>
2023-05-08 20:59:17 +0200

    reset wheels velocity on deactivate

--- diff_drive_controller/src/diff_drive_controller.cpp
+++ diff_drive_controller/src/diff_drive_controller.cpp
@@ -481,6 +481,7 @@ controller_interface::CallbackReturn DiffDriveController::on_deactivate(
   const rclcpp_lifecycle::State &)
 {
   subscriber_is_active_ = false;
+  halt();
   registered_left_wheel_handles_.clear();
   registered_right_wheel_handles_.clear();
   return controller_interface::CallbackReturn::SUCCESS;
