49d3f6d4322855780760f24f6c9cb174593270a2
Bence Magyar <bence.magyar.robotics@gmail.com>
2023-06-04 19:00:02 +0100

    Remove unnecessary include (#645)
    (cherry picked from commit a910b9d93395f6d8dd20d06d7aa5b95968cbf152)
    
    # Conflicts:
    #	steering_controllers_library/include/steering_controllers_library/steering_odometry.hpp

--- steering_controllers_library/include/steering_controllers_library/steering_odometry.hpp
+++ steering_controllers_library/include/steering_controllers_library/steering_odometry.hpp
@@ -24,8 +24,12 @@
 #include "realtime_tools/realtime_buffer.h"
 #include "realtime_tools/realtime_publisher.h"
 
+<<<<<<< HEAD
 #include <boost/function.hpp>
 #include "rcppmath/rolling_mean_accumulator.hpp"
+=======
+#include "rcpputils/rolling_mean_accumulator.hpp"
+>>>>>>> a910b9d (Remove unnecessary include (#645))
 
 namespace steering_odometry
 {
