8a56c6ed2e110e991cfed69c13309be43df82325
Bence Magyar <bence.magyar.robotics@gmail.com>
2023-06-23 20:16:25 +0100

    move comment to relevant code

--- joint_trajectory_controller/src/joint_trajectory_controller.cpp
+++ joint_trajectory_controller/src/joint_trajectory_controller.cpp
@@ -364,7 +364,6 @@ controller_interface::return_type JointTrajectoryController::update(
     {
       /// Likely an Empty trajectory from set_hold_position()
       /// Command hold position
-      // TODO(anyone): How to halt when using effort commands?
       for (size_t index = 0; index < dof_; ++index)
       {
         if (has_position_command_interface_)
@@ -377,7 +376,8 @@ controller_interface::return_type JointTrajectoryController::update(
         {
           joint_command_interface_[1][index].get().set_value(0.0);
         }
-
+        
+        // TODO(anyone): How to halt when using effort commands?
         if (has_effort_command_interface_)
         {
           joint_command_interface_[3][index].get().set_value(0.0);
