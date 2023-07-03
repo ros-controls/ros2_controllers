0f07f2775e8b5e1535a181ee620b42b5530041eb
Christoph Froehlich <christoph.froehlich@ait.ac.at>
2023-07-03 21:21:13 +0000

    Extend tests for set_hold_position

--- joint_trajectory_controller/test/test_trajectory_actions.cpp
+++ joint_trajectory_controller/test/test_trajectory_actions.cpp
@@ -243,6 +243,14 @@ TEST_P(TestTrajectoryActionsTestParameterized, test_success_single_point_sendgoa
   EXPECT_TRUE(gh_future.get());
   EXPECT_EQ(rclcpp_action::ResultCode::SUCCEEDED, common_resultcode_);
 
+  // run an update
+  updateController(rclcpp::Duration::from_seconds(0.01));
+
+  // it should be holding the last position goal
+  // i.e., active but trivial trajectory (one point only)
+  EXPECT_TRUE(traj_controller_->has_active_traj());
+  EXPECT_FALSE(traj_controller_->has_nontrivial_traj());
+
   if (traj_controller_->has_position_command_interface())
   {
     EXPECT_EQ(1.0, joint_pos_[0]);
@@ -293,6 +301,14 @@ TEST_P(TestTrajectoryActionsTestParameterized, test_success_multi_point_sendgoal
   EXPECT_TRUE(gh_future.get());
   EXPECT_EQ(rclcpp_action::ResultCode::SUCCEEDED, common_resultcode_);
 
+  // run an update
+  updateController(rclcpp::Duration::from_seconds(0.01));
+
+  // it should be holding the last position goal
+  // i.e., active but trivial trajectory (one point only)
+  EXPECT_TRUE(traj_controller_->has_active_traj());
+  EXPECT_FALSE(traj_controller_->has_nontrivial_traj());
+
   if (traj_controller_->has_position_command_interface())
   {
     EXPECT_NEAR(7.0, joint_pos_[0], COMMON_THRESHOLD);
@@ -337,6 +353,14 @@ TEST_F(TestTrajectoryActions, test_goal_tolerances_single_point_success)
   EXPECT_EQ(rclcpp_action::ResultCode::SUCCEEDED, common_resultcode_);
   EXPECT_EQ(
     control_msgs::action::FollowJointTrajectory_Result::SUCCESSFUL, common_action_result_code_);
+
+  // run an update
+  updateController(rclcpp::Duration::from_seconds(0.01));
+
+  // it should be holding the last position goal
+  // i.e., active but trivial trajectory (one point only)
+  EXPECT_TRUE(traj_controller_->has_active_traj());
+  EXPECT_FALSE(traj_controller_->has_nontrivial_traj());
 
   if (traj_controller_->has_position_command_interface())
   {
@@ -400,6 +424,14 @@ TEST_F(TestTrajectoryActions, test_goal_tolerances_multi_point_success)
   EXPECT_EQ(
     control_msgs::action::FollowJointTrajectory_Result::SUCCESSFUL, common_action_result_code_);
 
+  // run an update
+  updateController(rclcpp::Duration::from_seconds(0.01));
+
+  // it should be holding the last position goal
+  // i.e., active but trivial trajectory (one point only)
+  EXPECT_TRUE(traj_controller_->has_active_traj());
+  EXPECT_FALSE(traj_controller_->has_nontrivial_traj());
+
   if (traj_controller_->has_position_command_interface())
   {
     EXPECT_NEAR(7.0, joint_pos_[0], COMMON_THRESHOLD);
@@ -452,8 +484,13 @@ TEST_P(TestTrajectoryActionsTestParameterized, test_state_tolerances_fail)
     control_msgs::action::FollowJointTrajectory_Result::PATH_TOLERANCE_VIOLATED,
     common_action_result_code_);
 
-  // run an update, it should be holding
+  // run an update
   updateController(rclcpp::Duration::from_seconds(0.01));
+
+  // it should be holding the position (being the initial one)
+  // i.e., active but trivial trajectory (one point only)
+  EXPECT_TRUE(traj_controller_->has_active_traj());
+  EXPECT_FALSE(traj_controller_->has_nontrivial_traj());
 
   if (traj_controller_->has_position_command_interface())
   {
@@ -503,14 +540,19 @@ TEST_P(TestTrajectoryActionsTestParameterized, test_goal_tolerances_fail)
     control_msgs::action::FollowJointTrajectory_Result::GOAL_TOLERANCE_VIOLATED,
     common_action_result_code_);
 
-  // run an update, it should be holding the last received goal
+  // run an update
   updateController(rclcpp::Duration::from_seconds(0.01));
+
+  // it should be holding the position (being the initial one)
+  // i.e., active but trivial trajectory (one point only)
+  EXPECT_TRUE(traj_controller_->has_active_traj());
+  EXPECT_FALSE(traj_controller_->has_nontrivial_traj());
 
   if (traj_controller_->has_position_command_interface())
   {
-    EXPECT_NEAR(4.0, joint_pos_[0], COMMON_THRESHOLD);
-    EXPECT_NEAR(5.0, joint_pos_[1], COMMON_THRESHOLD);
-    EXPECT_NEAR(6.0, joint_pos_[2], COMMON_THRESHOLD);
+    EXPECT_NEAR(INITIAL_POS_JOINT1, joint_pos_[0], COMMON_THRESHOLD);
+    EXPECT_NEAR(INITIAL_POS_JOINT2, joint_pos_[1], COMMON_THRESHOLD);
+    EXPECT_NEAR(INITIAL_POS_JOINT3, joint_pos_[2], COMMON_THRESHOLD);
   }
 }
 
@@ -525,10 +567,6 @@ TEST_P(TestTrajectoryActionsTestParameterized, test_no_time_from_start_state_tol
 
   SetUpExecutor(params);
   SetUpControllerHardware();
-
-  const double init_pos1 = joint_pos_[0];
-  const double init_pos2 = joint_pos_[1];
-  const double init_pos3 = joint_pos_[2];
 
   std::shared_future<typename GoalHandle::SharedPtr> gh_future;
   // send goal
@@ -553,14 +591,19 @@ TEST_P(TestTrajectoryActionsTestParameterized, test_no_time_from_start_state_tol
     control_msgs::action::FollowJointTrajectory_Result::PATH_TOLERANCE_VIOLATED,
     common_action_result_code_);
 
-  // run an update, it should be holding
+  // run an update
   updateController(rclcpp::Duration::from_seconds(0.01));
+
+  // it should be holding the position (being the initial one)
+  // i.e., active but trivial trajectory (one point only)
+  EXPECT_TRUE(traj_controller_->has_active_traj());
+  EXPECT_FALSE(traj_controller_->has_nontrivial_traj());
 
   if (traj_controller_->has_position_command_interface())
   {
-    EXPECT_NEAR(init_pos1, joint_pos_[0], COMMON_THRESHOLD);
-    EXPECT_NEAR(init_pos2, joint_pos_[1], COMMON_THRESHOLD);
-    EXPECT_NEAR(init_pos3, joint_pos_[2], COMMON_THRESHOLD);
+    EXPECT_NEAR(INITIAL_POS_JOINT1, joint_pos_[0], COMMON_THRESHOLD);
+    EXPECT_NEAR(INITIAL_POS_JOINT2, joint_pos_[1], COMMON_THRESHOLD);
+    EXPECT_NEAR(INITIAL_POS_JOINT3, joint_pos_[2], COMMON_THRESHOLD);
   }
 }
 
@@ -605,8 +648,13 @@ TEST_P(TestTrajectoryActionsTestParameterized, test_cancel_hold_position)
   const double prev_pos2 = joint_pos_[1];
   const double prev_pos3 = joint_pos_[2];
 
-  // run an update, it should be holding
+  // run an update
   updateController(rclcpp::Duration::from_seconds(0.01));
+
+  // it should be holding the last position,
+  // i.e., active but trivial trajectory (one point only)
+  EXPECT_TRUE(traj_controller_->has_active_traj());
+  EXPECT_FALSE(traj_controller_->has_nontrivial_traj());
 
   if (traj_controller_->has_position_command_interface())
   {
