25ae7177ab3282677926257ead835091ba1d0a92
Christoph Fröhlich <christophfroehlich@users.noreply.github.com>
2023-11-19 20:17:24 +0100

    [JTC] Remove unused home pose (backport #845)

--- joint_trajectory_controller/include/joint_trajectory_controller/joint_trajectory_controller.hpp
+++ joint_trajectory_controller/include/joint_trajectory_controller/joint_trajectory_controller.hpp
@@ -183,8 +183,6 @@ protected:
   rclcpp::Service<control_msgs::srv::QueryTrajectoryState>::SharedPtr query_state_srv_;
 
   std::shared_ptr<Trajectory> traj_external_point_ptr_ = nullptr;
-  std::shared_ptr<Trajectory> traj_home_point_ptr_ = nullptr;
-  std::shared_ptr<trajectory_msgs::msg::JointTrajectory> traj_msg_home_ptr_ = nullptr;
   realtime_tools::RealtimeBuffer<std::shared_ptr<trajectory_msgs::msg::JointTrajectory>>
     traj_msg_external_point_ptr_;
 
