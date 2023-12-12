cf9ede69679a7d545740571b0d1dbb9d26f0da5e
Franz Rammerstorfer <franz.rammerstorfer@v2c2.at>
2023-12-12 11:41:25 +0100

    fix copyright
    according to pre-commit checks. sry!

--- steering_controllers_library/test/test_steering_odometry.cpp
+++ steering_controllers_library/test/test_steering_odometry.cpp
@@ -1,3 +1,4 @@
+// Copyright (c) 2023, Virtual Vehicle Research GmbH
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
