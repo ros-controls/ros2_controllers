4492c144a5f111db47e6c9bedba6555c6b09d370
Henry Moore <henry.moore2@technipfmc.com>
2024-08-13 22:03:46 +0000

    add skeleton of MTJTC

---
+++ multi_time_joint_trajectory_controller/include/multi_time_joint_trajectory_controller/multi_time_joint_trajectory_controller.hpp
@@ -0,0 +1,27 @@
+// Copyright (c) 2024 ros2_control Development Team
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#ifndef MULTI_TIME_JOINT_TRAJECTORY_CONTROLLER__MULTI_TIME_JOINT_TRAJECTORY_CONTROLLER_HPP_
+#define MULTI_TIME_JOINT_TRAJECTORY_CONTROLLER__MULTI_TIME_JOINT_TRAJECTORY_CONTROLLER_HPP_
+
+#include "controller_interface/controller_interface.hpp"
+
+namespace joint_trajectory_controller
+{
+
+class JointTrajectoryController : public controller_interface::ControllerInterface
+{
+};
+}  // namespace joint_trajectory_controller
+#endif  // MULTI_TIME_JOINT_TRAJECTORY_CONTROLLER__MULTI_TIME_JOINT_TRAJECTORY_CONTROLLER_HPP_
