d732440e6d34564056e76d4cfba35febcff1a84d
Henry Moore <henry.moore@picknik.ai>
2024-08-19 20:16:58 +0000

    flesh out skeleton of MTTC

---
+++ multi_time_trajectory_controller/include/multi_time_trajectory_controller/tolerances.hpp
@@ -0,0 +1,175 @@
+// Copyright 2013 PAL Robotics S.L.
+// All rights reserved.
+//
+// Software License Agreement (BSD License 2.0)
+// Redistribution and use in source and binary forms, with or without
+// modification, are permitted provided that the following conditions are met:
+//   * Redistributions of source code must retain the above copyright notice,
+//     this list of conditions and the following disclaimer.
+//   * Redistributions in binary form must reproduce the above copyright
+//     notice, this list of conditions and the following disclaimer in the
+//     documentation and/or other materials provided with the distribution.
+//   * Neither the name of PAL Robotics S.L. nor the names of its
+//     contributors may be used to endorse or promote products derived from
+//     this software without specific prior written permission.
+//
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+// AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+// IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+// ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
+// LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+// CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+// SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+// INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+// CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+// ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+// POSSIBILITY OF SUCH DAMAGE.
+
+/// \author Adolfo Rodriguez Tsouroukdissian
+
+#ifndef MULTI_TIME_TRAJECTORY_CONTROLLER__TOLERANCES_HPP_
+#define MULTI_TIME_TRAJECTORY_CONTROLLER__TOLERANCES_HPP_
+
+#include <vector>
+
+#include "control_msgs/msg/multi_time_trajectory_point.hpp"
+#include "multi_time_trajectory_controller_parameters.hpp"
+
+namespace multi_time_trajectory_controller
+{
+/**
+ * \brief Trajectory state tolerances for position, velocity and acceleration variables.
+ *
+ * A tolerance value of zero means that no tolerance will be applied for that variable.
+ */
+struct StateTolerances
+{
+  double position = 0.0;
+  double velocity = 0.0;
+  double acceleration = 0.0;
+};
+
+/**
+ * \brief Trajectory segment tolerances.
+ */
+struct SegmentTolerances
+{
+  explicit SegmentTolerances(size_t size = 0) : state_tolerance(size), goal_state_tolerance(size) {}
+
+  /** State tolerances that apply during segment execution. */
+  std::vector<StateTolerances> state_tolerance;
+
+  /** State tolerances that apply for the goal state only.*/
+  std::vector<StateTolerances> goal_state_tolerance;
+
+  /** Extra time after the segment end time allowed to reach the goal state tolerances. */
+  double goal_time_tolerance = 0.0;
+};
+
+/**
+ * \brief Populate trajectory segment tolerances using data from the ROS node.
+ *
+ * It is assumed that the following parameter structure is followed on the provided LifecycleNode.
+ * Unspecified parameters will take the defaults shown in the comments:
+ *
+ * \code
+ * constraints:
+ *  goal_time: 1.0                   # Defaults to zero
+ *  stopped_velocity_tolerance: 0.02 # Defaults to 0.01
+ *  foo_joint:
+ *    trajectory: 0.05               # Defaults to zero (ie. the tolerance is not enforced)
+ *    goal:       0.03               # Defaults to zero (ie. the tolerance is not enforced)
+ *  bar_joint:
+ *    goal: 0.01
+ * \endcode
+ *
+ * \param params The ROS Parameters
+ * \return Trajectory segment tolerances.
+ */
+SegmentTolerances get_segment_tolerances(Params const & params)
+{
+  auto const & constraints = params.constraints;
+  auto const n_joints = params.joints.size();
+
+  SegmentTolerances tolerances;
+  tolerances.goal_time_tolerance = constraints.goal_time;
+
+  // State and goal state tolerances
+  tolerances.state_tolerance.resize(n_joints);
+  tolerances.goal_state_tolerance.resize(n_joints);
+  for (size_t i = 0; i < n_joints; ++i)
+  {
+    auto const joint = params.joints[i];
+    tolerances.state_tolerance[i].position = constraints.joints_map.at(joint).trajectory;
+    tolerances.goal_state_tolerance[i].position = constraints.joints_map.at(joint).goal;
+    tolerances.goal_state_tolerance[i].velocity = constraints.stopped_velocity_tolerance;
+
+    auto logger = rclcpp::get_logger("tolerance");
+    RCLCPP_DEBUG(
+      logger, "%s %f", (joint + ".trajectory").c_str(), tolerances.state_tolerance[i].position);
+    RCLCPP_DEBUG(
+      logger, "%s %f", (joint + ".goal").c_str(), tolerances.goal_state_tolerance[i].position);
+  }
+
+  return tolerances;
+}
+
+/**
+ * \param state_error State error to check.
+ * \param joint_idx Joint index for the state error
+ * \param state_tolerance State tolerance of joint to check \p state_error against.
+ * \param show_errors If the joint that violate its tolerance should be output to console. NOT
+ * REALTIME if true \return True if \p state_error fulfills \p state_tolerance.
+ */
+inline bool check_state_tolerance_per_joint(
+  const control_msgs::msg::MultiTimeTrajectoryPoint & state_error, size_t joint_idx,
+  const StateTolerances & state_tolerance, bool show_errors = false)
+{
+  using std::abs;
+  const double error_position = state_error.positions[joint_idx];
+  const double error_velocity =
+    state_error.velocities.empty() ? 0.0 : state_error.velocities[joint_idx];
+  const double error_acceleration =
+    state_error.accelerations.empty() ? 0.0 : state_error.accelerations[joint_idx];
+
+  const bool is_valid =
+    !(state_tolerance.position > 0.0 && abs(error_position) > state_tolerance.position) &&
+    !(state_tolerance.velocity > 0.0 && abs(error_velocity) > state_tolerance.velocity) &&
+    !(state_tolerance.acceleration > 0.0 && abs(error_acceleration) > state_tolerance.acceleration);
+
+  if (is_valid)
+  {
+    return true;
+  }
+
+  if (show_errors)
+  {
+    const auto logger = rclcpp::get_logger("tolerances");
+    RCLCPP_ERROR(logger, "State tolerances failed for joint %lu:", joint_idx);
+
+    if (state_tolerance.position > 0.0 && abs(error_position) > state_tolerance.position)
+    {
+      RCLCPP_ERROR(
+        logger, "Position Error: %f, Position Tolerance: %f", error_position,
+        state_tolerance.position);
+    }
+    if (state_tolerance.velocity > 0.0 && abs(error_velocity) > state_tolerance.velocity)
+    {
+      RCLCPP_ERROR(
+        logger, "Velocity Error: %f, Velocity Tolerance: %f", error_velocity,
+        state_tolerance.velocity);
+    }
+    if (
+      state_tolerance.acceleration > 0.0 && abs(error_acceleration) > state_tolerance.acceleration)
+    {
+      RCLCPP_ERROR(
+        logger, "Acceleration Error: %f, Acceleration Tolerance: %f", error_acceleration,
+        state_tolerance.acceleration);
+    }
+  }
+  return false;
+}
+
+}  // namespace multi_time_trajectory_controller
+
+#endif  // MULTI_TIME_TRAJECTORY_CONTROLLER__TOLERANCES_HPP_
---
+++ multi_time_trajectory_controller/include/multi_time_trajectory_controller/trajectory.hpp
@@ -0,0 +1,226 @@
+// Copyright 2017 Open Source Robotics Foundation, Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#ifndef MULTI_TIME_TRAJECTORY_CONTROLLER__TRAJECTORY_HPP_
+#define MULTI_TIME_TRAJECTORY_CONTROLLER__TRAJECTORY_HPP_
+
+#include <memory>
+#include <vector>
+
+#include "control_msgs/msg/multi_time_trajectory.hpp"
+#include "control_msgs/msg/multi_time_trajectory_point.hpp"
+#include "joint_limits/joint_limiter_interface.hpp"
+#include "joint_limits/joint_limits.hpp"
+#include "joint_trajectory_controller/interpolation_methods.hpp"
+#include "rclcpp/time.hpp"
+#include "ruckig/ruckig.hpp"
+
+namespace multi_time_trajectory_controller
+{
+using TrajectoryPointIter = std::vector<control_msgs::msg::MultiTimeTrajectoryPoint>::iterator;
+using TrajectoryPointConstIter =
+  std::vector<control_msgs::msg::MultiTimeTrajectoryPoint>::const_iterator;
+
+class Trajectory
+{
+public:
+  Trajectory();
+
+  explicit Trajectory(std::shared_ptr<control_msgs::msg::MultiTimeTrajectory> trajectory);
+
+  explicit Trajectory(
+    const rclcpp::Time & current_time,
+    const control_msgs::msg::MultiTimeTrajectoryPoint & current_point,
+    std::shared_ptr<control_msgs::msg::MultiTimeTrajectory> trajectory);
+
+  /**
+   *  Set the point before the trajectory message is replaced/appended
+   * Example: if we receive a new trajectory message and it's first point is 0.5 seconds
+   * from the current one, we call this function to log the current state, then
+   * append/replace the current trajectory
+   * \param joints_angle_wraparound Vector of boolean where true value corresponds to a joint that
+   * wrap around (ie. is continuous).
+   */
+
+  void set_point_before_trajectory_msg(
+    const rclcpp::Time & current_time,
+    const control_msgs::msg::MultiTimeTrajectoryPoint & current_point,
+    const std::vector<bool> & joints_angle_wraparound = std::vector<bool>());
+
+  void update(
+    std::shared_ptr<control_msgs::msg::MultiTimeTrajectory> joint_trajectory,
+    const std::vector<joint_limits::JointLimits> & joint_limits, const rclcpp::Duration & period);
+
+  /// Find the segment (made up of 2 points) and its expected state from the
+  /// containing trajectory.
+  /**
+   * Sampling trajectory at given \p sample_time.
+   * If position in the \p end_segment_itr is missing it will be deduced from provided velocity, or
+   * acceleration respectively. Deduction assumes that the provided velocity or acceleration have to
+   * be reached at the time defined in the segment.
+   *
+   * Specific case returns for start_segment_itr and end_segment_itr:
+   * - Sampling before the trajectory start:
+   *   start_segment_itr = begin(), end_segment_itr = begin()
+   * - Sampling exactly on a point of the trajectory:
+   *    start_segment_itr = iterator where point is, end_segment_itr = iterator after
+   * start_segment_itr
+   * - Sampling between points:
+   *    start_segment_itr = iterator before the sampled point, end_segment_itr = iterator after
+   * start_segment_itr
+   * - Sampling after entire trajectory:
+   *    start_segment_itr = --end(), end_segment_itr = end()
+   * - Sampling empty msg or before the time given in set_point_before_trajectory_msg()
+   *    return false
+   *
+   * \param[in] sample_time Time at which trajectory will be sampled.
+   * \param[in] interpolation_method Specify whether splines, another method, or no interpolation at
+   * all. \param[out] expected_state Calculated new at \p sample_time. \param[out] start_segment_itr
+   * Iterator to the start segment for given \p sample_time. See description above. \param[out]
+   * end_segment_itr Iterator to the end segment for given \p sample_time. See description above.
+   */
+
+  bool sample(
+    const rclcpp::Time & sample_time,
+    const joint_trajectory_controller::interpolation_methods::InterpolationMethod
+      interpolation_method,
+    control_msgs::msg::MultiTimeTrajectoryPoint & output_state,
+    TrajectoryPointConstIter & start_segment_itr, TrajectoryPointConstIter & end_segment_itr,
+    const rclcpp::Duration & period,
+    std::unique_ptr<joint_limits::JointLimiterInterface<joint_limits::JointLimits>> & joint_limiter,
+    control_msgs::msg::MultiTimeTrajectoryPoint & splines_state,
+    control_msgs::msg::MultiTimeTrajectoryPoint & ruckig_state,
+    control_msgs::msg::MultiTimeTrajectoryPoint & ruckig_input_state);
+
+  /**
+   * Do interpolation between 2 states given a time in between their respective timestamps
+   *
+   * The start and end states need not necessarily be specified all the way to the acceleration
+   * level:
+   * - If only \b positions are specified, linear interpolation will be used.
+   * - If \b positions and \b velocities are specified, a cubic spline will be used.
+   * - If \b positions, \b velocities and \b accelerations are specified, a quintic spline will be
+   * used.
+   *
+   * If start and end states have different specifications
+   * (eg. start is position-only, end is position-velocity), the lowest common specification will be
+   * used (position-only in the example).
+   *
+   * \param[in] time_a Time at which the segment state equals \p state_a.
+   * \param[in] state_a State at \p time_a.
+   * \param[in] time_b Time at which the segment state equals \p state_b.
+   * \param[in] state_b State at time \p time_b.
+   * \param[in] sample_time The time to sample, between time_a and time_b.
+   * \param[out] output The state at \p sample_time.
+   */
+
+  bool interpolate_between_points(
+    const rclcpp::Time & time_a, const control_msgs::msg::MultiTimeTrajectoryPoint & state_a,
+    const rclcpp::Time & time_b, const control_msgs::msg::MultiTimeTrajectoryPoint & state_b,
+    const rclcpp::Time & sample_time, const bool do_ruckig_smoothing, const bool skip_splines,
+    control_msgs::msg::MultiTimeTrajectoryPoint & output, const rclcpp::Duration & period,
+    control_msgs::msg::MultiTimeTrajectoryPoint & splines_state,
+    control_msgs::msg::MultiTimeTrajectoryPoint & ruckig_state,
+    control_msgs::msg::MultiTimeTrajectoryPoint & ruckig_input_state);
+
+  TrajectoryPointConstIter begin() const;
+
+  TrajectoryPointConstIter end() const;
+
+  rclcpp::Time time_from_start() const;
+
+  bool has_trajectory_msg() const;
+
+  bool has_nontrivial_msg() const;
+
+  std::shared_ptr<control_msgs::msg::MultiTimeTrajectory> get_trajectory_msg() const
+  {
+    return trajectory_msg_;
+  }
+
+  bool is_sampled_already() const { return sampled_already_; }
+
+private:
+  void deduce_from_derivatives(
+    control_msgs::msg::MultiTimeTrajectoryPoint & first_state,
+    control_msgs::msg::MultiTimeTrajectoryPoint & second_state, const size_t dim,
+    const double delta_t);
+
+  std::shared_ptr<control_msgs::msg::MultiTimeTrajectory> trajectory_msg_;
+  rclcpp::Time trajectory_start_time_;
+
+  rclcpp::Time time_before_traj_msg_;
+  control_msgs::msg::MultiTimeTrajectoryPoint state_before_traj_msg_;
+
+  bool sampled_already_ = false;
+
+  // For Ruckig jerk-limited smoothing
+  std::unique_ptr<ruckig::Ruckig<ruckig::DynamicDOFs>> smoother_;
+  ruckig::InputParameter<ruckig::DynamicDOFs> ruckig_input_{0};
+  ruckig::OutputParameter<ruckig::DynamicDOFs> ruckig_output_{0};
+  // To avoid instability, Ruckig runs in a closed-loop fashion:
+  // Ruckig output at cycle i is used as the initial state for cycle i+1.
+  // This flag determines whether we need to initialize the state or use the previous
+  // Ruckig output.
+  bool have_previous_ruckig_output_ = false;
+
+  control_msgs::msg::MultiTimeTrajectoryPoint previous_state_;
+};
+
+/**
+ * \return The map between \p t1 indices (implicitly encoded in return vector indices) to \p t2
+ * indices. If \p t1 is <tt>"{C, B}"</tt> and \p t2 is <tt>"{A, B, C, D}"</tt>, the associated
+ * mapping vector is <tt>"{2, 1}"</tt>.
+ */
+template <class T>
+inline std::vector<size_t> mapping(const T & t1, const T & t2)
+{
+  // t1 must be a subset of t2
+  if (t1.size() > t2.size())
+  {
+    return std::vector<size_t>();
+  }
+
+  std::vector<size_t> mapping_vector(t1.size());  // Return value
+  for (auto t1_it = t1.begin(); t1_it != t1.end(); ++t1_it)
+  {
+    auto t2_it = std::find(t2.begin(), t2.end(), *t1_it);
+    if (t2.end() == t2_it)
+    {
+      return std::vector<size_t>();
+    }
+    else
+    {
+      const size_t t1_dist = std::distance(t1.begin(), t1_it);
+      const size_t t2_dist = std::distance(t2.begin(), t2_it);
+      mapping_vector[t1_dist] = t2_dist;
+    }
+  }
+  return mapping_vector;
+}
+
+/**
+ * \param current_position The current position given from the controller, which will be adapted.
+ * \param next_position Next position from which to compute the wraparound offset, i.e.,
+ *      the first trajectory point
+ * \param joints_angle_wraparound Vector of boolean where true value corresponds to a joint that
+ * wrap around (ie. is continuous).
+ */
+void wraparound_joint(
+  std::vector<double> & current_position, const std::vector<double> next_position,
+  const std::vector<bool> & joints_angle_wraparound);
+
+}  // namespace multi_time_trajectory_controller
+
+#endif  // MULTI_TIME_TRAJECTORY_CONTROLLER__TRAJECTORY_HPP_
