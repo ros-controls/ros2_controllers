cc4694a257102592b33b6c52b013fab715607397
Sanjeev Kumar <sanjeev.kumar@ipa.fraunhofer.de>
2024-09-02 21:29:43 +0200

    Fix deprecation warning in paramater declaration - implicitly
    Updated the paramater declaration to include type in `publisher_forward_position_controller` and `publisher_joint_trajectory_controller`
    
    Changes Made:
    - Replaced `self.declare_parameter(name)` with `self.declare_parameter(name, rclpy.Parameter.Type.DOUBLE_ARRAY)`

--- ros2_controllers_test_nodes/ros2_controllers_test_nodes/publisher_forward_position_controller.py
+++ ros2_controllers_test_nodes/ros2_controllers_test_nodes/publisher_forward_position_controller.py
@@ -17,7 +17,6 @@
 
 import rclpy
 from rclpy.node import Node
-from rcl_interfaces.msg import ParameterDescriptor
 
 from std_msgs.msg import Float64MultiArray
 
@@ -38,7 +37,7 @@ class PublisherForwardPosition(Node):
         # Read all positions from parameters
         self.goals = []
         for name in goal_names:
-            self.declare_parameter(name, descriptor=ParameterDescriptor(dynamic_typing=True))
+            self.declare_parameter(name, rclpy.Parameter.Type.DOUBLE_ARRAY)
             goal = self.get_parameter(name).value
             if goal is None or len(goal) == 0:
                 raise Exception(f'Values for goal "{name}" not set!')
