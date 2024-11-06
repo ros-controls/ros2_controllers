3dd20906906bae48ecc1287ea65fbf6eeff4b251
Christoph Froehlich <christoph.froehlich@ait.ac.at>
2024-11-06 22:08:04 +0000

    Fix API

--- admittance_controller/src/admittance_controller.cpp
+++ admittance_controller/src/admittance_controller.cpp
@@ -285,8 +285,7 @@ controller_interface::CallbackReturn AdmittanceController::on_configure(
 
   // configure admittance rule
   if (
-    admittance_->configure(get_node(), num_joints_, this->get_robot_description()) ==
-    controller_interface::return_type::ERROR)
+    admittance_->configure(get_node(), num_joints_, "") == controller_interface::return_type::ERROR)
   {
     return controller_interface::CallbackReturn::ERROR;
   }
