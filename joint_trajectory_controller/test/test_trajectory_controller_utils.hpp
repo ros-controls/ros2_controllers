777cf159d3d97c722405bb01fc2ffd3d358b161a
Christoph Froehlich <christoph.froehlich@ait.ac.at>
2024-11-08 21:34:04 +0000

    Fix API change from rolling

--- joint_trajectory_controller/test/test_trajectory_controller_utils.hpp
+++ joint_trajectory_controller/test/test_trajectory_controller_utils.hpp
@@ -364,9 +364,7 @@ public:
   {
     if (traj_controller_)
     {
-      if (
-        traj_controller_->get_lifecycle_state().id() ==
-        lifecycle_msgs::msg::State::PRIMARY_STATE_ACTIVE)
+      if (traj_controller_->get_state().id() == lifecycle_msgs::msg::State::PRIMARY_STATE_ACTIVE)
       {
         EXPECT_EQ(
           traj_controller_->get_node()->deactivate().id(),
