72b1c4e8e0d8cb903d8ebd3d6f50f80ec6a927f4
Sachin Kumar <sachinkum123567@gmail.com>
2025-01-10 00:32:10 +0100

    copyright update
    Co-authored-by: Dr. Denis <denis@stoglrobotics.de>

--- io_gripper_controller/include/io_gripper_controller/io_gripper_controller.hpp
+++ io_gripper_controller/include/io_gripper_controller/io_gripper_controller.hpp
@@ -1,4 +1,4 @@
-// Copyright (c) 2024, Stogl Robotics Consulting UG (haftungsbeschränkt)
+// Copyright (c) 2025, b>>robotized by Stogl Robotics
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
