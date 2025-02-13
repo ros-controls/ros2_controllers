820e8b6e88e4aab4a775577eee6401898d3b6ffa
Christoph Froehlich <christoph.froehlich@ait.ac.at>
2025-02-13 20:04:09 +0000

    Write tests for save_i_term behavior

--- pid_controller/test/test_pid_controller.hpp
+++ pid_controller/test/test_pid_controller.hpp
@@ -61,6 +61,8 @@ class TestablePidController : public pid_controller::PidController
   FRIEND_TEST(PidControllerTest, receive_message_and_publish_updated_status);
   FRIEND_TEST(PidControllerTest, test_update_chained_feedforward_with_gain);
   FRIEND_TEST(PidControllerTest, test_update_chained_feedforward_off_with_gain);
+  FRIEND_TEST(PidControllerTest, test_save_i_term_on);
+  FRIEND_TEST(PidControllerTest, test_save_i_term_off);
 
 public:
   controller_interface::CallbackReturn on_configure(
