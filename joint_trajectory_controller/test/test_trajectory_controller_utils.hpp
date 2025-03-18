ac35696493d4634413155145170aa6455ed832db
Felix Exner (fexner) <git@fexner.de>
2025-03-18 15:16:31 +0100

    [jtc tests] avoid dangling ref of command / state interfaces (#1596)

--- joint_trajectory_controller/test/test_trajectory_controller_utils.hpp
+++ joint_trajectory_controller/test/test_trajectory_controller_utils.hpp
@@ -259,6 +259,12 @@ public:
       controller_name_ + "/joint_trajectory", rclcpp::SystemDefaultsQoS());
   }
 
+  void TearDown() override
+  {
+    DeactivateTrajectoryController();
+    traj_controller_.reset();
+  }
+
   void SetUpTrajectoryController(
     rclcpp::Executor & executor, const std::vector<rclcpp::Parameter> & parameters = {},
     const std::string & urdf = ros2_control_test_assets::minimal_robot_urdf)
@@ -815,7 +821,7 @@ public:
     state_interface_types_ = std::get<1>(GetParam());
   }
 
-  virtual void TearDown() { TrajectoryControllerTest::DeactivateTrajectoryController(); }
+  virtual void TearDown() { TrajectoryControllerTest::TearDown(); }
 
   static void TearDownTestCase() { TrajectoryControllerTest::TearDownTestCase(); }
 };
