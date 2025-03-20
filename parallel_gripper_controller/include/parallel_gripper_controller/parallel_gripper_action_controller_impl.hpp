eca84fa7e741d9a466c07cc857b337228ab3370b
Vedant Randive <vedantsr3@gmail.com>
2025-03-20 15:07:00 +0530

    Replace RCLCPP_*_STREAM macros with RCLCPP_* (#1600)

--- parallel_gripper_controller/include/parallel_gripper_controller/parallel_gripper_action_controller_impl.hpp
+++ parallel_gripper_controller/include/parallel_gripper_controller/parallel_gripper_action_controller_impl.hpp
@@ -230,8 +230,8 @@ controller_interface::CallbackReturn GripperActionController::on_configure(
 
   // Action status checking update rate
   action_monitor_period_ = rclcpp::Duration::from_seconds(1.0 / params_.action_monitor_rate);
-  RCLCPP_INFO_STREAM(
-    logger, "Action status changes will be monitored at " << params_.action_monitor_rate << "Hz.");
+  RCLCPP_INFO(
+    logger, "Action status changes will be monitored at %f Hz.", params_.action_monitor_rate);
 
   // Controlled joint
   if (params_.joint.empty())
@@ -253,17 +253,16 @@ controller_interface::CallbackReturn GripperActionController::on_activate(
     { return command_interface.get_interface_name() == hardware_interface::HW_IF_POSITION; });
   if (command_interface_it == command_interfaces_.end())
   {
-    RCLCPP_ERROR_STREAM(
-      get_node()->get_logger(),
-      "Expected 1 " << hardware_interface::HW_IF_POSITION << " command interface");
+    RCLCPP_ERROR(
+      get_node()->get_logger(), "Expected 1 %s command interface",
+      hardware_interface::HW_IF_POSITION);
     return controller_interface::CallbackReturn::ERROR;
   }
   if (command_interface_it->get_prefix_name() != params_.joint)
   {
-    RCLCPP_ERROR_STREAM(
-      get_node()->get_logger(), "Command interface is different than joint name `"
-                                  << command_interface_it->get_prefix_name() << "` != `"
-                                  << params_.joint << "`");
+    RCLCPP_ERROR(
+      get_node()->get_logger(), "Command interface is different than joint name `%s` != `%s`",
+      command_interface_it->get_prefix_name().c_str(), params_.joint.c_str());
     return controller_interface::CallbackReturn::ERROR;
   }
   const auto position_state_interface_it = std::find_if(
@@ -277,10 +276,10 @@ controller_interface::CallbackReturn GripperActionController::on_activate(
   }
   if (position_state_interface_it->get_prefix_name() != params_.joint)
   {
-    RCLCPP_ERROR_STREAM(
-      get_node()->get_logger(), "Position state interface is different than joint name `"
-                                  << position_state_interface_it->get_prefix_name() << "` != `"
-                                  << params_.joint << "`");
+    RCLCPP_ERROR(
+      get_node()->get_logger(),
+      "Position state interface is different than joint name `%s` != `%s`",
+      position_state_interface_it->get_prefix_name().c_str(), params_.joint.c_str());
     return controller_interface::CallbackReturn::ERROR;
   }
   const auto velocity_state_interface_it = std::find_if(
@@ -294,10 +293,10 @@ controller_interface::CallbackReturn GripperActionController::on_activate(
   }
   if (velocity_state_interface_it->get_prefix_name() != params_.joint)
   {
-    RCLCPP_ERROR_STREAM(
-      get_node()->get_logger(), "Velocity command interface is different than joint name `"
-                                  << velocity_state_interface_it->get_prefix_name() << "` != `"
-                                  << params_.joint << "`");
+    RCLCPP_ERROR(
+      get_node()->get_logger(),
+      "Velocity command interface is different than joint name `%s` != `%s`",
+      velocity_state_interface_it->get_prefix_name().c_str(), params_.joint.c_str());
     return controller_interface::CallbackReturn::ERROR;
   }
 
