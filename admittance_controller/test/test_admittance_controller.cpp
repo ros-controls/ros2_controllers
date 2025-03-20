47b18bc64cfc5e660a614dd10e5425ba2282259d
rehanshah17 <rehans0906@gmail.com>
2025-03-19 22:48:20 -0400

    fixed the copy and paste #504 issue, added the test check_frame_ids_in_controller_state

--- admittance_controller/test/test_admittance_controller.cpp
+++ admittance_controller/test/test_admittance_controller.cpp
@@ -313,6 +313,26 @@ TEST_F(AdmittanceControllerTest, publish_status_success)
   //   }
 }
 
+TEST_F(AdmittanceControllerTest, check_frame_ids_in_controller_state)
+{
+  SetUpController();
+
+  ASSERT_EQ(controller_->on_configure(rclcpp_lifecycle::State()), NODE_SUCCESS);
+  ASSERT_EQ(controller_->on_activate(rclcpp_lifecycle::State()), NODE_SUCCESS);
+
+  broadcast_tfs(); // force torque sensor 
+
+  ASSERT_EQ(
+    controller_->update(rclcpp::Time(0), rclcpp::Duration::from_seconds(0.01)),
+    controller_interface::return_type::OK);
+
+  const auto& msg = controller_->admittance_->get_controller_state();
+
+  // Ensure correct frame IDs
+  ASSERT_EQ(msg.ref_trans_base_ft.header.frame_id, controller_->admittance_->parameters_.kinematics.base);
+  ASSERT_EQ(msg.ref_trans_base_ft.child_frame, controller_->admittance_->parameters_.ft_sensor.frame.id);
+}
+
 TEST_F(AdmittanceControllerTest, receive_message_and_publish_updated_status)
 {
   SetUpController();
