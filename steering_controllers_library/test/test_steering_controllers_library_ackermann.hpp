f9f245917ada6204d15dc06471098611977710d4
wittenator <9154515+wittenator@users.noreply.github.com>
2025-04-11 20:31:51 +0000

    Fix all pre-commit checks

--- steering_controllers_library/test/test_steering_controllers_library_ackermann.hpp
+++ steering_controllers_library/test/test_steering_controllers_library_ackermann.hpp
@@ -12,8 +12,8 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-#ifndef TEST_STEERING_CONTROLLERS_LIBRARY_HPP_
-#define TEST_STEERING_CONTROLLERS_LIBRARY_HPP_
+#ifndef TEST_STEERING_CONTROLLERS_LIBRARY_ACKERMANN_HPP_
+#define TEST_STEERING_CONTROLLERS_LIBRARY_ACKERMANN_HPP_
 
 #include <gmock/gmock.h>
 
@@ -340,4 +340,4 @@ protected:
   rclcpp::Publisher<ControllerAckermannReferenceMsg>::SharedPtr command_publisher_;
 };
 
-#endif  // TEST_STEERING_CONTROLLERS_LIBRARY_HPP_
+#endif  // TEST_STEERING_CONTROLLERS_LIBRARY_ACKERMANN_HPP_
