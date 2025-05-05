d9d4c67a3e9fc971991b7550b1b3839575c9151c
mathias31415 <mathias31415@gmx.de>
2025-05-05 16:54:33 +0200

    uncommented test_motion_primitives_forward_controller_preceeding implementation

--- motion_primitives_forward_controller/test/test_motion_primitives_forward_controller_preceeding.cpp
+++ motion_primitives_forward_controller/test/test_motion_primitives_forward_controller_preceeding.cpp
@@ -14,64 +14,64 @@
 //
 // Authors:
 
-// #include "test_motion_primitives_forward_controller.hpp"
+#include "test_motion_primitives_forward_controller.hpp"
 
-// #include <limits>
-// #include <memory>
-// #include <string>
-// #include <utility>
-// #include <vector>
+#include <limits>
+#include <memory>
+#include <string>
+#include <utility>
+#include <vector>
 
-// using motion_primitives_forward_controller::CMD_MY_ITFS;
-// using motion_primitives_forward_controller::control_mode_type;
-// using motion_primitives_forward_controller::STATE_MY_ITFS;
+using motion_primitives_forward_controller::CMD_MY_ITFS;
+using motion_primitives_forward_controller::control_mode_type;
+using motion_primitives_forward_controller::STATE_MY_ITFS;
 
-// class MotionPrimitivesForwardControllerTest : public MotionPrimitivesForwardControllerFixture<TestableMotionPrimitivesForwardController>
-// {
-// };
+class MotionPrimitivesForwardControllerTest : public MotionPrimitivesForwardControllerFixture<TestableMotionPrimitivesForwardController>
+{
+};
 
-// TEST_F(MotionPrimitivesForwardControllerTest, all_parameters_set_configure_success)
-// {
-//   SetUpController();
+TEST_F(MotionPrimitivesForwardControllerTest, all_parameters_set_configure_success)
+{
+  SetUpController();
 
-//   ASSERT_TRUE(controller_->params_.joints.empty());
-//   ASSERT_TRUE(controller_->params_.state_joints.empty());
-//   ASSERT_TRUE(controller_->params_.interface_name.empty());
+  ASSERT_TRUE(controller_->params_.joints.empty());
+  ASSERT_TRUE(controller_->params_.state_joints.empty());
+  ASSERT_TRUE(controller_->params_.interface_name.empty());
 
-//   ASSERT_EQ(controller_->on_configure(rclcpp_lifecycle::State()), NODE_SUCCESS);
+  ASSERT_EQ(controller_->on_configure(rclcpp_lifecycle::State()), NODE_SUCCESS);
 
-//   ASSERT_THAT(controller_->params_.joints, testing::ElementsAreArray(joint_names_));
-//   ASSERT_THAT(controller_->params_.state_joints, testing::ElementsAreArray(state_joint_names_));
-//   ASSERT_THAT(controller_->state_joints_, testing::ElementsAreArray(state_joint_names_));
-//   ASSERT_EQ(controller_->params_.interface_name, interface_name_);
-// }
+  ASSERT_THAT(controller_->params_.joints, testing::ElementsAreArray(joint_names_));
+  ASSERT_THAT(controller_->params_.state_joints, testing::ElementsAreArray(state_joint_names_));
+  ASSERT_THAT(controller_->state_joints_, testing::ElementsAreArray(state_joint_names_));
+  ASSERT_EQ(controller_->params_.interface_name, interface_name_);
+}
 
-// TEST_F(MotionPrimitivesForwardControllerTest, check_exported_intefaces)
-// {
-//   SetUpController();
+TEST_F(MotionPrimitivesForwardControllerTest, check_exported_intefaces)
+{
+  SetUpController();
 
-//   ASSERT_EQ(controller_->on_configure(rclcpp_lifecycle::State()), NODE_SUCCESS);
+  ASSERT_EQ(controller_->on_configure(rclcpp_lifecycle::State()), NODE_SUCCESS);
 
-//   auto command_intefaces = controller_->command_interface_configuration();
-//   ASSERT_EQ(command_intefaces.names.size(), joint_command_values_.size());
-//   for (size_t i = 0; i < command_intefaces.names.size(); ++i)
-//   {
-//     EXPECT_EQ(command_intefaces.names[i], joint_names_[i] + "/" + interface_name_);
-//   }
+  auto command_intefaces = controller_->command_interface_configuration();
+  ASSERT_EQ(command_intefaces.names.size(), joint_command_values_.size());
+  for (size_t i = 0; i < command_intefaces.names.size(); ++i)
+  {
+    EXPECT_EQ(command_intefaces.names[i], joint_names_[i] + "/" + interface_name_);
+  }
 
-//   auto state_intefaces = controller_->state_interface_configuration();
-//   ASSERT_EQ(state_intefaces.names.size(), joint_state_values_.size());
-//   for (size_t i = 0; i < state_intefaces.names.size(); ++i)
-//   {
-//     EXPECT_EQ(state_intefaces.names[i], state_joint_names_[i] + "/" + interface_name_);
-//   }
-// }
+  auto state_intefaces = controller_->state_interface_configuration();
+  ASSERT_EQ(state_intefaces.names.size(), joint_state_values_.size());
+  for (size_t i = 0; i < state_intefaces.names.size(); ++i)
+  {
+    EXPECT_EQ(state_intefaces.names[i], state_joint_names_[i] + "/" + interface_name_);
+  }
+}
 
-// int main(int argc, char ** argv)
-// {
-//   ::testing::InitGoogleTest(&argc, argv);
-//   rclcpp::init(argc, argv);
-//   int result = RUN_ALL_TESTS();
-//   rclcpp::shutdown();
-//   return result;
-// }
+int main(int argc, char ** argv)
+{
+  ::testing::InitGoogleTest(&argc, argv);
+  rclcpp::init(argc, argv);
+  int result = RUN_ALL_TESTS();
+  rclcpp::shutdown();
+  return result;
+}
