e76a8d07fa0cb470464e06f84bf3e78e22bdb4fb
mathias31415 <mathias31415@gmx.de>
2025-05-05 16:59:58 +0200

    changed test_motion_primitives_forward_controller_preceeding implementation from rtw template to fit the controller implementation

--- motion_primitives_forward_controller/test/test_motion_primitives_forward_controller_preceeding.cpp
+++ motion_primitives_forward_controller/test/test_motion_primitives_forward_controller_preceeding.cpp
@@ -12,7 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 //
-// Authors:
+// Authors: Mathias Fuhrer
 
 #include "test_motion_primitives_forward_controller.hpp"
 
@@ -23,7 +23,6 @@
 #include <vector>
 
 using motion_primitives_forward_controller::CMD_MY_ITFS;
-using motion_primitives_forward_controller::control_mode_type;
 using motion_primitives_forward_controller::STATE_MY_ITFS;
 
 class MotionPrimitivesForwardControllerTest : public MotionPrimitivesForwardControllerFixture<TestableMotionPrimitivesForwardController>
@@ -34,16 +33,15 @@ TEST_F(MotionPrimitivesForwardControllerTest, all_parameters_set_configure_succe
 {
   SetUpController();
 
-  ASSERT_TRUE(controller_->params_.joints.empty());
-  ASSERT_TRUE(controller_->params_.state_joints.empty());
-  ASSERT_TRUE(controller_->params_.interface_name.empty());
+  ASSERT_TRUE(controller_->params_.command_interfaces.empty());
+  ASSERT_TRUE(controller_->params_.state_interfaces.empty());
+  ASSERT_TRUE(controller_->params_.name.empty());
 
   ASSERT_EQ(controller_->on_configure(rclcpp_lifecycle::State()), NODE_SUCCESS);
 
-  ASSERT_THAT(controller_->params_.joints, testing::ElementsAreArray(joint_names_));
-  ASSERT_THAT(controller_->params_.state_joints, testing::ElementsAreArray(state_joint_names_));
-  ASSERT_THAT(controller_->state_joints_, testing::ElementsAreArray(state_joint_names_));
-  ASSERT_EQ(controller_->params_.interface_name, interface_name_);
+  ASSERT_THAT(controller_->params_.command_interfaces, testing::ElementsAreArray(command_interface_names_));
+  ASSERT_THAT(controller_->params_.state_interfaces, testing::ElementsAreArray(state_interface_names_));
+  ASSERT_EQ(controller_->params_.name, interface_namespace_);
 }
 
 TEST_F(MotionPrimitivesForwardControllerTest, check_exported_intefaces)
@@ -53,17 +51,17 @@ TEST_F(MotionPrimitivesForwardControllerTest, check_exported_intefaces)
   ASSERT_EQ(controller_->on_configure(rclcpp_lifecycle::State()), NODE_SUCCESS);
 
   auto command_intefaces = controller_->command_interface_configuration();
-  ASSERT_EQ(command_intefaces.names.size(), joint_command_values_.size());
+  ASSERT_EQ(command_intefaces.names.size(), command_values_.size());
   for (size_t i = 0; i < command_intefaces.names.size(); ++i)
   {
-    EXPECT_EQ(command_intefaces.names[i], joint_names_[i] + "/" + interface_name_);
+    EXPECT_EQ(command_intefaces.names[i], interface_namespace_ + "/" + command_interface_names_[i]);
   }
 
   auto state_intefaces = controller_->state_interface_configuration();
-  ASSERT_EQ(state_intefaces.names.size(), joint_state_values_.size());
+  ASSERT_EQ(state_intefaces.names.size(), state_values_.size());
   for (size_t i = 0; i < state_intefaces.names.size(); ++i)
   {
-    EXPECT_EQ(state_intefaces.names[i], state_joint_names_[i] + "/" + interface_name_);
+    EXPECT_EQ(state_intefaces.names[i], interface_namespace_ + "/" + state_interface_names_[i]);
   }
 }
 
