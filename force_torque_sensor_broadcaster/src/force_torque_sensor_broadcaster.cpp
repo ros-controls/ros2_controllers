7306d25106b72f16f5cc1156d9a7a35aa19744d6
Oscar Martinez <oscar.robotics@tutanota.com>
2025-07-16 10:38:34 +0200

    Removing unnecessary temporary objects created while calling emplace_back

--- force_torque_sensor_broadcaster/src/force_torque_sensor_broadcaster.cpp
+++ force_torque_sensor_broadcaster/src/force_torque_sensor_broadcaster.cpp
@@ -253,38 +253,32 @@ ForceTorqueSensorBroadcaster::on_export_state_interfaces()
   if (!force_names[0].empty())
   {
     exported_state_interfaces.emplace_back(
-      hardware_interface::StateInterface(
-        export_prefix, force_names[0], &realtime_raw_publisher_->msg_.wrench.force.x));
+      export_prefix, force_names[0], &realtime_raw_publisher_->msg_.wrench.force.x);
   }
   if (!force_names[1].empty())
   {
     exported_state_interfaces.emplace_back(
-      hardware_interface::StateInterface(
-        export_prefix, force_names[1], &realtime_raw_publisher_->msg_.wrench.force.y));
+      export_prefix, force_names[1], &realtime_raw_publisher_->msg_.wrench.force.y);
   }
   if (!force_names[2].empty())
   {
     exported_state_interfaces.emplace_back(
-      hardware_interface::StateInterface(
-        export_prefix, force_names[2], &realtime_raw_publisher_->msg_.wrench.force.z));
+      export_prefix, force_names[2], &realtime_raw_publisher_->msg_.wrench.force.z);
   }
   if (!torque_names[0].empty())
   {
     exported_state_interfaces.emplace_back(
-      hardware_interface::StateInterface(
-        export_prefix, torque_names[0], &realtime_raw_publisher_->msg_.wrench.torque.x));
+      export_prefix, torque_names[0], &realtime_raw_publisher_->msg_.wrench.torque.x);
   }
   if (!torque_names[1].empty())
   {
     exported_state_interfaces.emplace_back(
-      hardware_interface::StateInterface(
-        export_prefix, torque_names[1], &realtime_raw_publisher_->msg_.wrench.torque.y));
+      export_prefix, torque_names[1], &realtime_raw_publisher_->msg_.wrench.torque.y);
   }
   if (!torque_names[2].empty())
   {
     exported_state_interfaces.emplace_back(
-      hardware_interface::StateInterface(
-        export_prefix, torque_names[2], &realtime_raw_publisher_->msg_.wrench.torque.z));
+      export_prefix, torque_names[2], &realtime_raw_publisher_->msg_.wrench.torque.z);
   }
   return exported_state_interfaces;
 }
