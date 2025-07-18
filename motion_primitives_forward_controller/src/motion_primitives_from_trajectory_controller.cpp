b47e7eab36b43fbb8f9a7089ccfa7189192c8b20
mathias31415 <mathias31415@gmx.de>
2025-07-18 08:57:52 +0200

    Moprim_from_traj_controller: pass full interface names to the controller (such as $(var tf_prefix)motion_primitive/q1)

--- motion_primitives_forward_controller/src/motion_primitives_from_trajectory_controller.cpp
+++ motion_primitives_forward_controller/src/motion_primitives_from_trajectory_controller.cpp
@@ -137,7 +137,7 @@ MotionPrimitivesFromTrajectoryController::command_interface_configuration() cons
   // Iterate over all command interfaces from the config yaml file
   for (const auto & interface_name : params_.command_interfaces)
   {
-    command_interfaces_config.names.push_back("motion_primitive/" + interface_name);
+    command_interfaces_config.names.push_back(interface_name);
   }
   return command_interfaces_config;
 }
@@ -153,7 +153,7 @@ MotionPrimitivesFromTrajectoryController::state_interface_configuration() const
   // Iterate over all state interfaces from the config yaml file
   for (const auto & interface_name : params_.state_interfaces)
   {
-    state_interfaces_config.names.push_back("motion_primitive/" + interface_name);
+    state_interfaces_config.names.push_back(interface_name);
   }
   return state_interfaces_config;
 }
