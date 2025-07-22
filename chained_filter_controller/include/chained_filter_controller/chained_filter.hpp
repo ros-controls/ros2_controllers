c5a63f850e87d70a5ef4da3d93e528f0658083d6
Christoph Froehlich <christoph.froehlich@ait.ac.at>
2025-07-22 11:16:00 +0000

    Satisfy linters

--- chained_filter_controller/include/chained_filter_controller/chained_filter.hpp
+++ chained_filter_controller/include/chained_filter_controller/chained_filter.hpp
@@ -1,3 +1,17 @@
+// Copyright 2025 ros2_control PMC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 #ifndef CHAINED_FILTER_CONTROLLER__CHAINED_FILTER_HPP_
 #define CHAINED_FILTER_CONTROLLER__CHAINED_FILTER_HPP_
 
@@ -48,4 +62,4 @@ protected:
   double output_state_value_;
 };
 }  // namespace chained_filter_controller
-#endif
+#endif  // CHAINED_FILTER_CONTROLLER__CHAINED_FILTER_HPP_
--- chained_filter_controller/src/chained_filter.cpp
+++ chained_filter_controller/src/chained_filter.cpp
@@ -1,12 +1,22 @@
-// chained_filter.cpp (migrated from ROSCon 2024 workshop)
+// Copyright 2025 ros2_control PMC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
 
 #include "chained_filter_controller/chained_filter.hpp"
 
 #include <rclcpp/version.h>
 #include <limits>
 #include <pluginlib/class_list_macros.hpp>
-
-using namespace chained_filter;
 
 namespace chained_filter_controller
 {
