87f312bc4c99309350775b79785e5948f8dc6596
Mathias Fuhrer <148789048+mathias31415@users.noreply.github.com>
2025-08-29 13:11:55 +0200

    Refactoring to motion_primitives_base_controller (#1857)
    (cherry picked from commit dc2ee703ecc5adf3251228d65dca49d72b6f6359)

--- motion_primitives_controllers/include/motion_primitives_controllers/motion_primitives_base_controller.hpp
+++ motion_primitives_controllers/include/motion_primitives_controllers/motion_primitives_base_controller.hpp
@@ -14,15 +14,14 @@
 //
 // Authors: Mathias Fuhrer
 
-#ifndef MOTION_PRIMITIVES_FORWARD_CONTROLLER__MOTION_PRIMITIVES_FORWARD_CONTROLLER_HPP_
-#define MOTION_PRIMITIVES_FORWARD_CONTROLLER__MOTION_PRIMITIVES_FORWARD_CONTROLLER_HPP_
+#ifndef MOTION_PRIMITIVES_CONTROLLERS__MOTION_PRIMITIVES_BASE_CONTROLLER_HPP_
+#define MOTION_PRIMITIVES_CONTROLLERS__MOTION_PRIMITIVES_BASE_CONTROLLER_HPP_
 
 #include <chrono>
 #include <memory>
 #include <string>
 #include <vector>
 
-#include <motion_primitives_forward_controller/motion_primitives_forward_controller_parameters.hpp>
 #include <realtime_tools/lock_free_queue.hpp>
 #include <realtime_tools/realtime_server_goal_handle.hpp>
 #include "controller_interface/controller_interface.hpp"
@@ -32,11 +31,10 @@
 #include "realtime_tools/realtime_publisher.hpp"
 #include "realtime_tools/realtime_thread_safe_box.hpp"
 
-#include "control_msgs/action/execute_motion_primitive_sequence.hpp"
 #include "control_msgs/msg/motion_primitive.hpp"
 #include "rclcpp_action/rclcpp_action.hpp"
 
-namespace motion_primitives_forward_controller
+namespace motion_primitives_controllers
 {
 enum class ExecutionState : uint8_t
 {
@@ -64,10 +62,10 @@ enum class ReadyForNewPrimitive : uint8_t
   READY = 1
 };
 
-class MotionPrimitivesForwardController : public controller_interface::ControllerInterface
+class MotionPrimitivesBaseController : public controller_interface::ControllerInterface
 {
 public:
-  MotionPrimitivesForwardController();
+  MotionPrimitivesBaseController();
 
   controller_interface::CallbackReturn on_init() override;
 
@@ -88,23 +86,11 @@ public:
     const rclcpp::Time & time, const rclcpp::Duration & period) override;
 
 protected:
-  std::shared_ptr<motion_primitives_forward_controller::ParamListener> param_listener_;
-  motion_primitives_forward_controller::Params params_;
   std::string tf_prefix_;
 
   using MotionPrimitive = control_msgs::msg::MotionPrimitive;
   realtime_tools::LockFreeSPSCQueue<MotionPrimitive, 1024> moprim_queue_;
 
-  using ExecuteMotionAction = control_msgs::action::ExecuteMotionPrimitiveSequence;
-  rclcpp_action::Server<ExecuteMotionAction>::SharedPtr action_server_;
-  rclcpp_action::GoalResponse goal_received_callback(
-    const rclcpp_action::GoalUUID & uuid, std::shared_ptr<const ExecuteMotionAction::Goal> goal);
-  rclcpp_action::CancelResponse goal_cancelled_callback(
-    const std::shared_ptr<rclcpp_action::ServerGoalHandle<ExecuteMotionAction>> goal_handle);
-  void goal_accepted_callback(
-    std::shared_ptr<rclcpp_action::ServerGoalHandle<ExecuteMotionAction>> goal_handle);
-  using RealtimeGoalHandle = realtime_tools::RealtimeServerGoalHandle<ExecuteMotionAction>;
-  realtime_tools::RealtimeThreadSafeBox<std::shared_ptr<RealtimeGoalHandle>> rt_goal_handle_;
   std::atomic<bool> has_active_goal_ = false;
   rclcpp::TimerBase::SharedPtr goal_handle_timer_;
   rclcpp::Duration action_monitor_period_ = rclcpp::Duration(std::chrono::milliseconds(20));
@@ -123,6 +109,6 @@ protected:
   MotionPrimitive current_moprim_;
 };
 
-}  // namespace motion_primitives_forward_controller
+}  // namespace motion_primitives_controllers
 
-#endif  // MOTION_PRIMITIVES_FORWARD_CONTROLLER__MOTION_PRIMITIVES_FORWARD_CONTROLLER_HPP_
+#endif  // MOTION_PRIMITIVES_CONTROLLERS__MOTION_PRIMITIVES_BASE_CONTROLLER_HPP_
