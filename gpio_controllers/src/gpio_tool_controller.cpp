9741d8c1141206bb2cc9b5e5e11b780696ab57bb
Dr. Denis <denis.stogl@b-robotized.com>
2025-09-03 11:27:25 +0200

    Fix the time substraciton issues.

--- gpio_controllers/src/gpio_tool_controller.cpp
+++ gpio_controllers/src/gpio_tool_controller.cpp
@@ -312,7 +312,7 @@ controller_interface::return_type GpioToolController::update(
     {
       // do nothing
       state_change_start_ = time;
-      check_tool_state(time, false);
+      check_tool_state(state_change_start_, false);
       break;
     }
     case ToolAction::DISENGAGING:
