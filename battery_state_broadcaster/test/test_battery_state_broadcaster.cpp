39fc8d4ae662eb9f724675c7d01417b0b4378a2d
Yara Shahin <yara.shahin@b-robotized.com>
2025-09-05 02:36:41 +0300

    Add battery_state_broadcaster

---
+++ battery_state_broadcaster/test/test_battery_state_broadcaster.cpp
@@ -0,0 +1,369 @@
+// Copyright (c) 2025, b-robotized Group
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#include <limits>
+#include <memory>
+#include <string>
+#include <utility>
+#include <vector>
+
+#include "rclcpp/rclcpp.hpp"
+#include "test_battery_state_broadcaster.hpp"
+
+// Test correct broadcaster initialization
+TEST_F(BatteryStateBroadcasterTest, init_success) { SetUpBatteryStateBroadcaster(); }
+
+// Test that BatteryStateBroadcaster parses parameters correctly,
+// Test that BatteryStateBroadcaster aggregates interfaces and battery properties correctly,
+// sets up state interfaces on configure, and computes aggregated counts/sums.
+TEST_F(BatteryStateBroadcasterTest, all_parameters_set_configure_success)
+{
+  SetUpBatteryStateBroadcaster();
+
+  ASSERT_TRUE(battery_state_broadcaster_->params_.state_joints.empty());
+
+  ASSERT_EQ(battery_state_broadcaster_->on_configure(rclcpp_lifecycle::State()), NODE_SUCCESS);
+
+  ASSERT_THAT(
+    battery_state_broadcaster_->state_joints_, testing::ElementsAreArray(state_joint_names_));
+
+  auto interface_params = battery_state_broadcaster_->params_.interfaces.state_joints_map;
+  auto properties = battery_state_broadcaster_->params_.state_joints_map;
+  EXPECT_EQ(interface_params.at("left_wheel").battery_temperature, true);
+  EXPECT_EQ(interface_params.at("left_wheel").battery_current, false);
+  EXPECT_EQ(interface_params.at("left_wheel").battery_charge, true);
+  EXPECT_EQ(interface_params.at("left_wheel").battery_percentage, false);
+  EXPECT_EQ(interface_params.at("left_wheel").battery_power_supply_status, true);
+  EXPECT_EQ(interface_params.at("left_wheel").battery_power_supply_health, true);
+  EXPECT_EQ(interface_params.at("left_wheel").battery_present, false);
+
+  EXPECT_EQ(interface_params.at("right_wheel").battery_temperature, true);
+  EXPECT_EQ(interface_params.at("right_wheel").battery_current, true);
+  EXPECT_EQ(interface_params.at("right_wheel").battery_charge, true);
+  EXPECT_EQ(interface_params.at("right_wheel").battery_percentage, true);
+  EXPECT_EQ(interface_params.at("right_wheel").battery_power_supply_status, true);
+  EXPECT_EQ(interface_params.at("right_wheel").battery_power_supply_health, true);
+  EXPECT_EQ(interface_params.at("right_wheel").battery_present, false);
+
+  EXPECT_EQ(properties.at("left_wheel").minimum_voltage, 0.0);
+  EXPECT_EQ(properties.at("left_wheel").maximum_voltage, 10.0);
+  EXPECT_EQ(properties.at("left_wheel").capacity, 12000.0);
+  EXPECT_EQ(properties.at("left_wheel").design_capacity, 13000.0);
+  EXPECT_EQ(properties.at("left_wheel").power_supply_technology, 3);
+  EXPECT_EQ(properties.at("left_wheel").location, "left_slot");
+  EXPECT_EQ(properties.at("left_wheel").serial_number, "left_serial_device");
+
+  EXPECT_EQ(properties.at("right_wheel").minimum_voltage, 0.0);
+  EXPECT_EQ(properties.at("right_wheel").maximum_voltage, 15.0);
+  EXPECT_EQ(properties.at("right_wheel").capacity, 17000.0);
+  EXPECT_EQ(properties.at("right_wheel").design_capacity, 18000.0);
+  EXPECT_EQ(properties.at("right_wheel").power_supply_technology, 3);
+  EXPECT_EQ(properties.at("right_wheel").location, "right_slot");
+  EXPECT_EQ(properties.at("right_wheel").serial_number, "right_serial_device");
+
+  // check property aggregation
+  EXPECT_EQ(battery_state_broadcaster_->counts_.temperature_cnt, 2.0);
+  EXPECT_EQ(battery_state_broadcaster_->counts_.current_cnt, 1.0);
+  EXPECT_EQ(
+    battery_state_broadcaster_->counts_.percentage_cnt,
+    2.0);  // because min and max voltage are valid
+  EXPECT_EQ(battery_state_broadcaster_->sums_.capacity_sum, 29000.0);
+  EXPECT_EQ(battery_state_broadcaster_->sums_.design_capacity_sum, 31000.0);
+
+  // check interface configuration
+  auto cmd_if_conf = battery_state_broadcaster_->command_interface_configuration();
+  ASSERT_THAT(cmd_if_conf.names, IsEmpty());
+  EXPECT_EQ(cmd_if_conf.type, controller_interface::interface_configuration_type::NONE);
+  auto state_if_conf = battery_state_broadcaster_->state_interface_configuration();
+  ASSERT_THAT(state_if_conf.names, SizeIs(12lu));
+  EXPECT_EQ(state_if_conf.type, controller_interface::interface_configuration_type::INDIVIDUAL);
+}
+
+// check fails when no defined interfaces
+TEST_F(BatteryStateBroadcasterTest, no_interfaces_set_activate_fail)
+{
+  ASSERT_EQ(
+    battery_state_broadcaster_->init(
+      "test_battery_state_broadcaster", "", 0, "",
+      battery_state_broadcaster_->define_custom_node_options()),
+    controller_interface::return_type::OK);
+
+  ASSERT_EQ(battery_state_broadcaster_->on_configure(rclcpp_lifecycle::State()), NODE_SUCCESS);
+  ASSERT_EQ(battery_state_broadcaster_->on_activate(rclcpp_lifecycle::State()), NODE_FAILURE);
+}
+
+// check all msgs initial values
+// check logic for combined strings for local & serial number and same or none for power supply
+TEST_F(BatteryStateBroadcasterTest, activate_success)
+{
+  SetUpBatteryStateBroadcaster();
+
+  ASSERT_EQ(battery_state_broadcaster_->on_configure(rclcpp_lifecycle::State()), NODE_SUCCESS);
+  ASSERT_EQ(battery_state_broadcaster_->on_activate(rclcpp_lifecycle::State()), NODE_SUCCESS);
+
+  // check that the message is reset
+  RawBatteryStatesMsg raw_battery_states_msg =
+    battery_state_broadcaster_->raw_battery_states_realtime_publisher_->msg_;
+  EXPECT_EQ(raw_battery_states_msg.battery_states.size(), static_cast<size_t>(2));
+
+  // --- Left wheel ---
+  const auto & left = raw_battery_states_msg.battery_states[0];
+  EXPECT_EQ(left.header.frame_id, "left_wheel");
+  EXPECT_TRUE(std::isnan(left.voltage));
+  EXPECT_TRUE(std::isnan(left.temperature));
+  EXPECT_TRUE(std::isnan(left.current));
+  EXPECT_TRUE(std::isnan(left.charge));
+  EXPECT_FLOAT_EQ(left.capacity, 12000.0f);
+  EXPECT_FLOAT_EQ(left.design_capacity, 13000.0f);
+  EXPECT_TRUE(std::isnan(left.percentage));
+  EXPECT_EQ(left.power_supply_status, BatteryState::POWER_SUPPLY_STATUS_UNKNOWN);
+  EXPECT_EQ(left.power_supply_health, BatteryState::POWER_SUPPLY_HEALTH_UNKNOWN);
+  EXPECT_EQ(left.power_supply_technology, 3);
+  EXPECT_TRUE(left.present);
+  EXPECT_TRUE(left.cell_voltage.empty());
+  EXPECT_TRUE(left.cell_temperature.empty());
+  EXPECT_EQ(left.location, "left_slot");
+  EXPECT_EQ(left.serial_number, "left_serial_device");
+
+  // --- Right wheel ---
+  const auto & right = raw_battery_states_msg.battery_states[1];
+  EXPECT_EQ(right.header.frame_id, "right_wheel");
+  EXPECT_TRUE(std::isnan(right.voltage));
+  EXPECT_TRUE(std::isnan(right.temperature));
+  EXPECT_TRUE(std::isnan(right.current));
+  EXPECT_TRUE(std::isnan(right.charge));
+  EXPECT_FLOAT_EQ(right.capacity, 17000.0f);
+  EXPECT_FLOAT_EQ(right.design_capacity, 18000.0f);
+  EXPECT_TRUE(std::isnan(right.percentage));
+  EXPECT_EQ(right.power_supply_status, BatteryState::POWER_SUPPLY_STATUS_UNKNOWN);
+  EXPECT_EQ(right.power_supply_health, BatteryState::POWER_SUPPLY_HEALTH_UNKNOWN);
+  EXPECT_EQ(right.power_supply_technology, 3);
+  EXPECT_TRUE(right.present);
+  EXPECT_TRUE(right.cell_voltage.empty());
+  EXPECT_TRUE(right.cell_temperature.empty());
+  EXPECT_EQ(right.location, "right_slot");
+  EXPECT_EQ(right.serial_number, "right_serial_device");
+
+  BatteryStateMsg battery_state_msg =
+    battery_state_broadcaster_->battery_state_realtime_publisher_->msg_;
+
+  EXPECT_TRUE(std::isnan(battery_state_msg.voltage));
+  EXPECT_TRUE(std::isnan(battery_state_msg.temperature));
+  EXPECT_TRUE(std::isnan(battery_state_msg.current));
+  EXPECT_TRUE(std::isnan(battery_state_msg.charge));
+  EXPECT_TRUE(std::isnan(battery_state_msg.percentage));
+  EXPECT_DOUBLE_EQ(battery_state_msg.capacity, 29000.0);
+  EXPECT_DOUBLE_EQ(battery_state_msg.design_capacity, 31000.0);
+  EXPECT_EQ(battery_state_msg.power_supply_status, BatteryState::POWER_SUPPLY_STATUS_UNKNOWN);
+  EXPECT_EQ(battery_state_msg.power_supply_health, BatteryState::POWER_SUPPLY_HEALTH_UNKNOWN);
+  EXPECT_EQ(battery_state_msg.power_supply_technology, BatteryState::POWER_SUPPLY_TECHNOLOGY_LIPO);
+  EXPECT_EQ(battery_state_msg.location, "left_slot, right_slot, ");
+  EXPECT_EQ(battery_state_msg.serial_number, "left_serial_device, right_serial_device, ");
+  EXPECT_TRUE(battery_state_msg.present);
+  EXPECT_TRUE(battery_state_msg.cell_voltage.empty());
+  EXPECT_TRUE(battery_state_msg.cell_temperature.empty());
+}
+
+TEST_F(BatteryStateBroadcasterTest, deactivate_success)
+{
+  SetUpBatteryStateBroadcaster();
+
+  ASSERT_EQ(battery_state_broadcaster_->on_configure(rclcpp_lifecycle::State()), NODE_SUCCESS);
+  ASSERT_EQ(battery_state_broadcaster_->on_activate(rclcpp_lifecycle::State()), NODE_SUCCESS);
+  ASSERT_EQ(battery_state_broadcaster_->on_deactivate(rclcpp_lifecycle::State()), NODE_SUCCESS);
+}
+
+TEST_F(BatteryStateBroadcasterTest, check_exported_intefaces)
+{
+  SetUpBatteryStateBroadcaster();
+
+  ASSERT_EQ(battery_state_broadcaster_->on_configure(rclcpp_lifecycle::State()), NODE_SUCCESS);
+
+  auto command_intefaces = battery_state_broadcaster_->command_interface_configuration();
+  ASSERT_EQ(command_intefaces.names.size(), static_cast<size_t>(0));
+
+  auto state_interfaces = battery_state_broadcaster_->state_interface_configuration();
+  ASSERT_EQ(state_interfaces.names.size(), itfs_values_.size());
+  EXPECT_EQ(state_interfaces.names[0], "left_wheel/battery_voltage");
+  EXPECT_EQ(state_interfaces.names[1], "left_wheel/battery_temperature");
+  EXPECT_EQ(state_interfaces.names[2], "left_wheel/battery_charge");
+  EXPECT_EQ(state_interfaces.names[3], "left_wheel/battery_power_supply_status");
+  EXPECT_EQ(state_interfaces.names[4], "left_wheel/battery_power_supply_health");
+  EXPECT_EQ(state_interfaces.names[5], "right_wheel/battery_voltage");
+  EXPECT_EQ(state_interfaces.names[6], "right_wheel/battery_temperature");
+  EXPECT_EQ(state_interfaces.names[7], "right_wheel/battery_current");
+  EXPECT_EQ(state_interfaces.names[8], "right_wheel/battery_charge");
+  EXPECT_EQ(state_interfaces.names[9], "right_wheel/battery_percentage");
+  EXPECT_EQ(state_interfaces.names[10], "right_wheel/battery_power_supply_status");
+  EXPECT_EQ(state_interfaces.names[11], "right_wheel/battery_power_supply_health");
+}
+
+TEST_F(BatteryStateBroadcasterTest, update_success)
+{
+  SetUpBatteryStateBroadcaster();
+
+  ASSERT_EQ(battery_state_broadcaster_->on_configure(rclcpp_lifecycle::State()), NODE_SUCCESS);
+  ASSERT_EQ(battery_state_broadcaster_->on_activate(rclcpp_lifecycle::State()), NODE_SUCCESS);
+
+  ASSERT_EQ(
+    battery_state_broadcaster_->update(rclcpp::Time(0), rclcpp::Duration::from_seconds(0.01)),
+    controller_interface::return_type::OK);
+}
+
+// check correct values published
+// check correct aggregation for present, percentage, averages, and sums, and higher criticality
+TEST_F(BatteryStateBroadcasterTest, publish_status_success)
+{
+  SetUpBatteryStateBroadcaster();
+
+  ASSERT_EQ(battery_state_broadcaster_->on_configure(rclcpp_lifecycle::State()), NODE_SUCCESS);
+  ASSERT_EQ(battery_state_broadcaster_->on_activate(rclcpp_lifecycle::State()), NODE_SUCCESS);
+
+  ASSERT_EQ(
+    battery_state_broadcaster_->update(rclcpp::Time(0), rclcpp::Duration::from_seconds(0.01)),
+    controller_interface::return_type::OK);
+
+  RawBatteryStatesMsg raw_battery_states_msg;
+  BatteryStateMsg battery_state_msg;
+  subscribe_and_get_messages(raw_battery_states_msg, battery_state_msg);
+
+  ASSERT_EQ(raw_battery_states_msg.battery_states.size(), 2u);
+
+  // Left wheel
+  const auto & left = raw_battery_states_msg.battery_states[0];
+  EXPECT_EQ(left.header.frame_id, "left_wheel");
+  EXPECT_DOUBLE_EQ(left.voltage, 5.0);
+  EXPECT_DOUBLE_EQ(left.temperature, 60.0);
+  EXPECT_TRUE(std::isnan(left.current));  // disabled in params
+  EXPECT_DOUBLE_EQ(left.charge, 6000.0);
+  EXPECT_DOUBLE_EQ(left.capacity, 12000.0);
+  EXPECT_DOUBLE_EQ(left.design_capacity, 13000.0);
+  // percentage calculated (no interface) = (5.0 - 0.0) * 100 / (10.0 - 0.0) = 50
+  EXPECT_DOUBLE_EQ(left.percentage, 50.0);
+  EXPECT_EQ(left.power_supply_status, 3);  // from itfs_values_[3]
+  EXPECT_EQ(left.power_supply_health, 0);  // from itfs_values_[4]
+  EXPECT_EQ(left.power_supply_technology, BatteryState::POWER_SUPPLY_TECHNOLOGY_LIPO);
+  EXPECT_TRUE(left.present);  // voltage > 0.0
+  EXPECT_EQ(left.location, "left_slot");
+  EXPECT_EQ(left.serial_number, "left_serial_device");
+
+  // Right wheel
+  const auto & right = raw_battery_states_msg.battery_states[1];
+  EXPECT_EQ(right.header.frame_id, "right_wheel");
+  EXPECT_DOUBLE_EQ(right.voltage, 10.0);
+  EXPECT_DOUBLE_EQ(right.temperature, 80.0);
+  EXPECT_DOUBLE_EQ(right.current, 2000.0);
+  EXPECT_DOUBLE_EQ(right.charge, 5000.0);
+  EXPECT_DOUBLE_EQ(right.capacity, 17000.0);
+  EXPECT_DOUBLE_EQ(right.design_capacity, 18000.0);
+  EXPECT_DOUBLE_EQ(right.percentage, 66.0);  // directly from itfs_values_[9]
+  EXPECT_EQ(right.power_supply_status, 2);   // from itfs_values_[10]
+  EXPECT_EQ(right.power_supply_health, 4);   // from itfs_values_[11]
+  EXPECT_EQ(right.power_supply_technology, BatteryState::POWER_SUPPLY_TECHNOLOGY_LIPO);
+  EXPECT_TRUE(right.present);  // voltage > 0.0
+  EXPECT_EQ(right.location, "right_slot");
+  EXPECT_EQ(right.serial_number, "right_serial_device");
+
+  // Combined battery state message
+  EXPECT_EQ(battery_state_msg.header.frame_id, "");
+  EXPECT_DOUBLE_EQ(battery_state_msg.voltage, 7.5);              // average of 5 + 10
+  EXPECT_DOUBLE_EQ(battery_state_msg.temperature, 70.0);         // average of 60 + 80
+  EXPECT_DOUBLE_EQ(battery_state_msg.current, 2000.0);           // only right wheel contributes
+  EXPECT_DOUBLE_EQ(battery_state_msg.charge, 11000.0);           // sum of 6000 + 5000
+  EXPECT_DOUBLE_EQ(battery_state_msg.capacity, 29000.0);         // sum of 6000 + 5000
+  EXPECT_DOUBLE_EQ(battery_state_msg.design_capacity, 31000.0);  // sum of 6000 + 5000
+  EXPECT_DOUBLE_EQ(battery_state_msg.percentage, 58.0);          // average of 50 + 66
+  EXPECT_EQ(battery_state_msg.power_supply_status, 3);           // max(3, 2)
+  EXPECT_EQ(battery_state_msg.power_supply_health, 4);           // max(0, 4)
+  EXPECT_EQ(battery_state_msg.power_supply_technology, BatteryState::POWER_SUPPLY_TECHNOLOGY_LIPO);
+  EXPECT_TRUE(battery_state_msg.present);  // voltage > 0.0
+  EXPECT_EQ(battery_state_msg.location, "left_slot, right_slot, ");
+  EXPECT_EQ(battery_state_msg.serial_number, "left_serial_device, right_serial_device, ");
+}
+
+TEST_F(BatteryStateBroadcasterTest, update_broadcasted_success)
+{
+  SetUpBatteryStateBroadcaster();
+
+  ASSERT_EQ(battery_state_broadcaster_->on_configure(rclcpp_lifecycle::State()), NODE_SUCCESS);
+  ASSERT_EQ(battery_state_broadcaster_->on_activate(rclcpp_lifecycle::State()), NODE_SUCCESS);
+
+  ASSERT_TRUE(left_voltage_itf_.set_value(10.0));
+
+  RawBatteryStatesMsg raw_battery_states_msg;
+  BatteryStateMsg battery_state_msg;
+  subscribe_and_get_messages(raw_battery_states_msg, battery_state_msg);
+
+  ASSERT_EQ(raw_battery_states_msg.battery_states.size(), 2u);
+
+  // Left wheel
+  const auto & left = raw_battery_states_msg.battery_states[0];
+  EXPECT_DOUBLE_EQ(left.voltage, 10.0);
+  // percentage calculated (no interface) = (10.0 - 0.0) * 100 / (10.0 - 0.0) = 50
+  EXPECT_DOUBLE_EQ(left.percentage, 100.0);
+  EXPECT_TRUE(left.present);  // voltage > 0.0
+
+  // Right wheel
+  const auto & right = raw_battery_states_msg.battery_states[1];
+  EXPECT_DOUBLE_EQ(right.voltage, 10.0);
+  EXPECT_DOUBLE_EQ(right.percentage, 66.0);  // directly from itfs_values_[9]
+  EXPECT_TRUE(right.present);                // voltage > 0.0
+
+  // Combined battery state message
+  EXPECT_DOUBLE_EQ(battery_state_msg.voltage, 10.0);     // average of 10 + 10
+  EXPECT_DOUBLE_EQ(battery_state_msg.percentage, 83.0);  // average of 100 + 66
+  EXPECT_TRUE(battery_state_msg.present);                // voltage > 0.0
+}
+
+TEST_F(BatteryStateBroadcasterTest, publish_nan_voltage)
+{
+  SetUpBatteryStateBroadcaster();
+
+  ASSERT_EQ(battery_state_broadcaster_->on_configure(rclcpp_lifecycle::State()), NODE_SUCCESS);
+  ASSERT_EQ(battery_state_broadcaster_->on_activate(rclcpp_lifecycle::State()), NODE_SUCCESS);
+
+  ASSERT_TRUE(left_voltage_itf_.set_value(std::numeric_limits<double>::quiet_NaN()));
+
+  RawBatteryStatesMsg raw_battery_states_msg;
+  BatteryStateMsg battery_state_msg;
+  subscribe_and_get_messages(raw_battery_states_msg, battery_state_msg);
+
+  ASSERT_EQ(raw_battery_states_msg.battery_states.size(), 2u);
+
+  // Left wheel
+  const auto & left = raw_battery_states_msg.battery_states[0];
+  EXPECT_TRUE(std::isnan(left.voltage));
+  EXPECT_TRUE(std::isnan(left.percentage));
+  EXPECT_FALSE(left.present);  // voltage nan
+
+  // Right wheel
+  const auto & right = raw_battery_states_msg.battery_states[1];
+  EXPECT_DOUBLE_EQ(right.voltage, 10.0);
+  EXPECT_DOUBLE_EQ(right.percentage, 66.0);  // directly from itfs_values_[9]
+  EXPECT_TRUE(right.present);                // voltage > 0.0
+
+  // Combined battery state message
+  EXPECT_TRUE(std::isnan(battery_state_msg.voltage));     // average of nan + 10
+  EXPECT_TRUE(std::isnan(battery_state_msg.percentage));  // average of nan + 66
+  EXPECT_TRUE(battery_state_msg.present);
+}
+
+int main(int argc, char ** argv)
+{
+  ::testing::InitGoogleTest(&argc, argv);
+  rclcpp::init(argc, argv);
+  int result = RUN_ALL_TESTS();
+  rclcpp::shutdown();
+  return result;
+}
---
+++ battery_state_broadcaster/test/test_battery_state_broadcaster.hpp
@@ -0,0 +1,205 @@
+// Copyright (c) 2025, b-robotized Group
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#ifndef TEST_BATTERY_STATE_BROADCASTER_HPP_
+#define TEST_BATTERY_STATE_BROADCASTER_HPP_
+
+#include <chrono>
+#include <limits>
+#include <memory>
+#include <string>
+#include <tuple>
+#include <utility>
+#include <vector>
+
+#include "battery_state_broadcaster/battery_state_broadcaster.hpp"
+#include "gmock/gmock.h"
+#include "hardware_interface/loaned_command_interface.hpp"
+#include "hardware_interface/loaned_state_interface.hpp"
+#include "hardware_interface/types/hardware_interface_return_values.hpp"
+#include "rclcpp/executor.hpp"
+#include "rclcpp/parameter_value.hpp"
+#include "rclcpp/time.hpp"
+#include "rclcpp/utilities.hpp"
+#include "rclcpp_lifecycle/node_interfaces/lifecycle_node_interface.hpp"
+
+#include "control_msgs/msg/battery_states.hpp"
+#include "sensor_msgs/msg/battery_state.hpp"
+
+using BatteryStateMsg = sensor_msgs::msg::BatteryState;
+using RawBatteryStatesMsg = control_msgs::msg::BatteryStates;
+using sensor_msgs::msg::BatteryState;
+using testing::IsEmpty;
+using testing::SizeIs;
+
+namespace
+{
+constexpr auto NODE_SUCCESS = controller_interface::CallbackReturn::SUCCESS;
+constexpr auto NODE_ERROR = controller_interface::CallbackReturn::ERROR;
+constexpr auto NODE_FAILURE = controller_interface::CallbackReturn::FAILURE;
+}  // namespace
+
+// subclassing and friending so we can access member variables
+class FriendBatteryStateBroadcaster : public battery_state_broadcaster::BatteryStateBroadcaster
+{
+  FRIEND_TEST(BatteryStateBroadcasterTest, init_success);
+  FRIEND_TEST(BatteryStateBroadcasterTest, all_parameters_set_configure_success);
+  FRIEND_TEST(BatteryStateBroadcasterTest, no_interfaces_set_activate_fail);
+  FRIEND_TEST(BatteryStateBroadcasterTest, activate_success);
+  FRIEND_TEST(BatteryStateBroadcasterTest, deactivate_success);
+  FRIEND_TEST(BatteryStateBroadcasterTest, check_exported_intefaces);
+  FRIEND_TEST(BatteryStateBroadcasterTest, update_success);
+  FRIEND_TEST(BatteryStateBroadcasterTest, publish_status_success);
+  FRIEND_TEST(BatteryStateBroadcasterTest, update_broadcasted_success);
+  FRIEND_TEST(BatteryStateBroadcasterTest, publish_nan_voltage);
+};
+
+class BatteryStateBroadcasterTest : public ::testing::Test
+{
+public:
+  static void SetUpTestCase() {}
+  static void TearDownTestCase() {}
+
+  void SetUp()
+  {
+    // initialize controller
+    battery_state_broadcaster_ = std::make_unique<FriendBatteryStateBroadcaster>();
+  }
+  void TearDown() { battery_state_broadcaster_.reset(nullptr); }
+
+  void SetUpBatteryStateBroadcaster(
+    const std::string controller_name = "test_battery_state_broadcaster")
+  {
+    ASSERT_EQ(
+      battery_state_broadcaster_->init(
+        controller_name, "", 0, "", battery_state_broadcaster_->define_custom_node_options()),
+      controller_interface::return_type::OK);
+
+    std::vector<hardware_interface::LoanedStateInterface> state_ifs;
+
+    state_ifs.emplace_back(left_voltage_itf_);
+    state_ifs.emplace_back(left_temperature_itf_);
+    state_ifs.emplace_back(left_charge_itf_);
+    state_ifs.emplace_back(left_status_itf_);
+    state_ifs.emplace_back(left_health_itf_);
+
+    state_ifs.emplace_back(right_voltage_itf_);
+    state_ifs.emplace_back(right_temperature_itf_);
+    state_ifs.emplace_back(right_current_itf_);
+    state_ifs.emplace_back(right_charge_itf_);
+    state_ifs.emplace_back(right_percentage_itf_);
+    state_ifs.emplace_back(right_status_itf_);
+    state_ifs.emplace_back(right_health_itf_);
+
+    battery_state_broadcaster_->assign_interfaces({}, std::move(state_ifs));
+  }
+
+protected:
+  // Controller-related parameters
+  std::vector<std::string> state_joint_names_ = {"left_wheel", "right_wheel"};
+  std::array<double, 12> itfs_values_ = {
+    5.0,     // 0 left_voltage
+    60.0,    // 1 left_temperature
+    6000.0,  // 2 left_charge
+    3.0,     // 3 left_status
+    0.0,     // 4 left_health
+    10.0,    // 5 right_voltage
+    80.0,    // 6 right_temperature
+    2000.0,  // 7 right_current
+    5000.0,  // 8 right_charge
+    66.0,    // 9 right_percentage
+    2.0,     // 10 right_status
+    4.0      // 11 right_health
+  };
+  // std::array<double, 13> itfs_values_ = {{1.1, 2.2, 3.3, 4.4, 5.5, 6.6, 7.7, 8.8, 9.9, 10.10}};
+
+  hardware_interface::StateInterface left_voltage_itf_{
+    "left_wheel", "battery_voltage", &itfs_values_[0]};
+  hardware_interface::StateInterface left_temperature_itf_{
+    "left_wheel", "battery_temperature", &itfs_values_[1]};
+  hardware_interface::StateInterface left_charge_itf_{
+    "left_wheel", "battery_charge", &itfs_values_[2]};
+  hardware_interface::StateInterface left_status_itf_{
+    "left_wheel", "battery_power_supply_status", &itfs_values_[3]};
+  hardware_interface::StateInterface left_health_itf_{
+    "left_wheel", "battery_power_supply_health", &itfs_values_[4]};
+  hardware_interface::StateInterface right_voltage_itf_{
+    "right_wheel", "battery_voltage", &itfs_values_[5]};
+  hardware_interface::StateInterface right_temperature_itf_{
+    "right_wheel", "battery_temperature", &itfs_values_[6]};
+  hardware_interface::StateInterface right_current_itf_{
+    "right_wheel", "battery_current", &itfs_values_[7]};
+  hardware_interface::StateInterface right_charge_itf_{
+    "right_wheel", "battery_charge", &itfs_values_[8]};
+  hardware_interface::StateInterface right_percentage_itf_{
+    "right_wheel", "battery_percentage", &itfs_values_[9]};
+  hardware_interface::StateInterface right_status_itf_{
+    "right_wheel", "battery_power_supply_status", &itfs_values_[10]};
+  hardware_interface::StateInterface right_health_itf_{
+    "right_wheel", "battery_power_supply_health", &itfs_values_[11]};
+
+  // Test related parameters
+  std::unique_ptr<FriendBatteryStateBroadcaster> battery_state_broadcaster_;
+
+  void subscribe_and_get_messages(
+    RawBatteryStatesMsg & raw_battery_states_msg, BatteryStateMsg & battery_state_msg)
+  {
+    // create a new subscriber
+    RawBatteryStatesMsg::SharedPtr received_raw_battery_states_msg;
+    BatteryStateMsg::SharedPtr received_battery_state_msg;
+    rclcpp::Node test_subscription_node("test_subscription_node");
+    auto raw_battery_states_callback = [&](const RawBatteryStatesMsg::SharedPtr msg)
+    { received_raw_battery_states_msg = msg; };
+    auto battery_state_callback = [&](const BatteryStateMsg::SharedPtr msg)
+    { received_battery_state_msg = msg; };
+    auto raw_battery_states_subscription =
+      test_subscription_node.create_subscription<RawBatteryStatesMsg>(
+        "/test_battery_state_broadcaster/raw_battery_states", 10, raw_battery_states_callback);
+    auto battery_state_subscription = test_subscription_node.create_subscription<BatteryStateMsg>(
+      "/test_battery_state_broadcaster/battery_state", 10, battery_state_callback);
+    rclcpp::executors::SingleThreadedExecutor executor;
+    executor.add_node(test_subscription_node.get_node_base_interface());
+
+    // call update to publish the test value
+    // since update doesn't guarantee a published message, republish until received
+    int max_sub_check_loop_count = 5;  // max number of tries for pub/sub loop
+    while (max_sub_check_loop_count--)
+    {
+      battery_state_broadcaster_->update(rclcpp::Time(0), rclcpp::Duration::from_seconds(0.01));
+      const auto timeout = std::chrono::milliseconds{5};
+      const auto until = test_subscription_node.get_clock()->now() + timeout;
+      while ((!received_battery_state_msg || !received_raw_battery_states_msg) &&
+             test_subscription_node.get_clock()->now() < until)
+      {
+        executor.spin_some();
+        std::this_thread::sleep_for(std::chrono::microseconds(10));
+      }
+      // check if message has been received
+      if (received_raw_battery_states_msg.get() && received_battery_state_msg.get())
+      {
+        break;
+      }
+    }
+    ASSERT_GE(max_sub_check_loop_count, 0) << "Test was unable to publish a message through "
+                                              "controller/broadcaster update loop";
+    ASSERT_TRUE(received_raw_battery_states_msg);
+    ASSERT_TRUE(received_battery_state_msg);
+
+    // take message from subscription
+    raw_battery_states_msg = *received_raw_battery_states_msg;
+    battery_state_msg = *received_battery_state_msg;
+  }
+};
+
+#endif  // TEST_BATTERY_STATE_BROADCASTER_HPP_
