2e3b3ab2cfe2989920297a0b0ba86ff111f731a0
Christoph Froehlich <christoph.froehlich@ait.ac.at>
2025-09-18 15:28:03 +0000

    Add _USE_MATH_DEFINES for trajectory class

--- joint_trajectory_controller/src/trajectory.cpp
+++ joint_trajectory_controller/src/trajectory.cpp
@@ -12,6 +12,8 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+#define _USE_MATH_DEFINES
+
 #include "joint_trajectory_controller/trajectory.hpp"
 
 #include <memory>
