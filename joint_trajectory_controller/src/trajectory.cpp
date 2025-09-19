b085a710b82788e59b8d11615c4cfdb1104bc35d
Christoph Froehlich <christoph.froehlich@ait.ac.at>
2025-09-19 18:27:16 +0000

    Set _USE_MATH_DEFINES globally instead

--- joint_trajectory_controller/src/trajectory.cpp
+++ joint_trajectory_controller/src/trajectory.cpp
@@ -12,8 +12,6 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-#define _USE_MATH_DEFINES
-
 #include "joint_trajectory_controller/trajectory.hpp"
 
 #include <memory>
