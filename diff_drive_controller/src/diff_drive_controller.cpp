a95a450e9167628c5da53ac0611cbc7672f7ef96
Aarav Gupta <aarav@spikonado.com>
2025-09-24 11:26:01 +0530

    Fix pre-commit

--- diff_drive_controller/src/diff_drive_controller.cpp
+++ diff_drive_controller/src/diff_drive_controller.cpp
@@ -667,13 +667,15 @@ DiffDriveController::on_export_reference_interfaces()
   std::vector<hardware_interface::CommandInterface> reference_interfaces;
   reference_interfaces.reserve(reference_interfaces_.size());
 
-  reference_interfaces.push_back(hardware_interface::CommandInterface(
-    get_node()->get_name() + std::string("/linear"), hardware_interface::HW_IF_VELOCITY,
-    &reference_interfaces_[0]));
+  reference_interfaces.push_back(
+      hardware_interface::CommandInterface(
+        get_node()->get_name() + std::string("/linear"), hardware_interface::HW_IF_VELOCITY,
+        &reference_interfaces_[0]));
 
-  reference_interfaces.push_back(hardware_interface::CommandInterface(
-    get_node()->get_name() + std::string("/angular"), hardware_interface::HW_IF_VELOCITY,
-    &reference_interfaces_[1]));
+  reference_interfaces.push_back(
+      hardware_interface::CommandInterface(
+        get_node()->get_name() + std::string("/angular"), hardware_interface::HW_IF_VELOCITY,
+        &reference_interfaces_[1]));
 
   return reference_interfaces;
 }
