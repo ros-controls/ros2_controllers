348e34e1bd4eb8f6b7e48719f98b5e63e3a803fc
Sanjeev Kumar <kumar.san96@gmail.com>
2025-11-18 17:03:16 +0100

    Remove implementation redirection from steering odometry header and add to source file for ABI

--- steering_controllers_library/include/steering_controllers_library/steering_odometry.hpp
+++ steering_controllers_library/include/steering_controllers_library/steering_odometry.hpp
@@ -60,101 +60,61 @@ inline bool is_close_to_zero(double val) { return steering_kinematics::is_close_
 class [[deprecated("Use steering_kinematics::SteeringKinematics instead")]] SteeringOdometry
 {
 public:
-  explicit SteeringOdometry(size_t velocity_rolling_window_size = 10)
-  : sk_impl_(velocity_rolling_window_size)
-  {
-  }
+  explicit SteeringOdometry(size_t velocity_rolling_window_size = 10);
 
-  void init(const rclcpp::Time & time) { sk_impl_.init(time); }
+  void init(const rclcpp::Time & time);
 
   bool update_from_position(
-    const double traction_wheel_pos, const double steer_pos, const double dt)
-  {
-    sk_impl_.update_from_position(traction_wheel_pos, steer_pos, dt);
-  }
+    const double traction_wheel_pos, const double steer_pos, const double dt);
 
   bool update_from_position(
     const double right_traction_wheel_pos, const double left_traction_wheel_pos,
-    const double steer_pos, const double dt)
-  {
-    sk_impl_.update_from_position(right_traction_wheel_pos, left_traction_wheel_pos, steer_pos, dt);
-  }
+    const double steer_pos, const double dt);
 
   bool update_from_position(
     const double right_traction_wheel_pos, const double left_traction_wheel_pos,
-    const double right_steer_pos, const double left_steer_pos, const double dt)
-  {
-    sk_impl_.update_from_position(
-      right_traction_wheel_pos, left_traction_wheel_pos, right_steer_pos, left_steer_pos, dt);
-  }
+    const double right_steer_pos, const double left_steer_pos, const double dt);
 
   bool update_from_velocity(
-    const double traction_wheel_vel, const double steer_pos, const double dt)
-  {
-    sk_impl_.update_from_velocity(traction_wheel_vel, steer_pos, dt);
-  }
+    const double traction_wheel_vel, const double steer_pos, const double dt);
 
   bool update_from_velocity(
     const double right_traction_wheel_vel, const double left_traction_wheel_vel,
-    const double steer_pos, const double dt)
-  {
-    sk_impl_.update_from_velocity(right_traction_wheel_vel, left_traction_wheel_vel, steer_pos, dt);
-  }
+    const double steer_pos, const double dt);
 
   bool update_from_velocity(
     const double right_traction_wheel_vel, const double left_traction_wheel_vel,
-    const double right_steer_pos, const double left_steer_pos, const double dt)
-  {
-    sk_impl_.update_from_velocity(
-      right_traction_wheel_vel, left_traction_wheel_vel, right_steer_pos, left_steer_pos, dt);
-  }
+    const double right_steer_pos, const double left_steer_pos, const double dt);
 
-  void update_open_loop(const double v_bx, const double omega_bz, const double dt)
-  {
-    sk_impl_.update_open_loop(v_bx, omega_bz, dt);
-  }
+  void update_open_loop(const double v_bx, const double omega_bz, const double dt);
 
-  void set_odometry_type(const unsigned int type) { sk_impl_.set_odometry_type(type); }
+  void set_odometry_type(const unsigned int type);
 
-  unsigned int get_odometry_type() const { sk_impl_.get_odometry_type(); }
+  unsigned int get_odometry_type() const;
+  double get_heading() const;
 
-  double get_heading() const { sk_impl_.get_heading(); }
+  double get_x() const;
 
-  double get_x() const { sk_impl_.get_x(); }
+  double get_y() const;
 
-  double get_y() const { sk_impl_.get_y(); }
+  double get_linear() const;
 
-  double get_linear() const { sk_impl_.get_linear(); }
-
-  double get_angular() const { sk_impl_.get_angular(); }
+  double get_angular() const;
 
   void set_wheel_params(
-    const double wheel_radius, const double wheel_base = 0.0, const double wheel_track = 0.0)
-  {
-    sk_impl_.set_wheel_params(wheel_radius, wheel_base, wheel_track);
-  }
+    const double wheel_radius, const double wheel_base, const double wheel_track);
 
   void set_wheel_params(
     const double wheel_radius, const double wheel_base, const double wheel_track_steering,
-    const double wheel_track_traction)
-  {
-    sk_impl_.set_wheel_params(wheel_radius, wheel_track_steering, wheel_track_traction);
-  }
+    const double wheel_track_traction);
 
-  void set_velocity_rolling_window_size(const size_t velocity_rolling_window_size)
-  {
-    sk_impl_.set_velocity_rolling_window_size(velocity_rolling_window_size);
-  }
+  void set_velocity_rolling_window_size(const size_t velocity_rolling_window_size);
 
   std::tuple<std::vector<double>, std::vector<double>> get_commands(
     double v_bx, double omega_bz, bool open_loop = true,
-    bool reduce_wheel_speed_until_steering_reached = false)
-  {
-    return sk_impl_.get_commands(
-      v_bx, omega_bz, open_loop, reduce_wheel_speed_until_steering_reached);
-  }
+    bool reduce_wheel_speed_until_steering_reached = false);
 
-  void reset_odometry() { sk_impl_.reset_odometry(); }
+  void reset_odometry();
 
 private:
   steering_kinematics::SteeringKinematics sk_impl_;
---
+++ steering_controllers_library/src/steering_odometry.cpp
@@ -0,0 +1,113 @@
+// Copyright (c) 2023, Stogl Robotics Consulting UG (haftungsbeschränkt)
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+/*
+ * Author: dr. sc. Tomislav Petkovic
+ * Author: Dr. Ing. Denis Stogl
+ */
+
+#define _USE_MATH_DEFINES
+
+#include "steering_controllers_library/steering_odometry.hpp"
+
+namespace steering_odometry
+{
+SteeringOdometry::SteeringOdometry(size_t velocity_rolling_window_size)
+: sk_impl_(velocity_rolling_window_size)
+{
+}
+
+void SteeringOdometry::init(const rclcpp::Time & time) { sk_impl_.init(time); }
+
+bool SteeringOdometry::update_from_position(
+  const double traction_wheel_pos, const double steer_pos, const double dt)
+{
+  return sk_impl_.update_from_position(traction_wheel_pos, steer_pos, dt);
+}
+
+bool SteeringOdometry::update_from_position(
+  const double traction_right_wheel_pos, const double traction_left_wheel_pos,
+  const double steer_pos, const double dt)
+{
+  return sk_impl_.update_from_position(
+    traction_right_wheel_pos, traction_left_wheel_pos, steer_pos, dt);
+}
+
+bool SteeringOdometry::update_from_position(
+  const double traction_right_wheel_pos, const double traction_left_wheel_pos,
+  const double right_steer_pos, const double left_steer_pos, const double dt)
+{
+  return sk_impl_.update_from_position(
+    traction_right_wheel_pos, traction_left_wheel_pos, right_steer_pos, left_steer_pos, dt);
+}
+
+bool SteeringOdometry::update_from_velocity(
+  const double traction_wheel_vel, const double steer_pos, const double dt)
+{
+  return sk_impl_.update_from_velocity(traction_wheel_vel, steer_pos, dt);
+}
+
+bool SteeringOdometry::update_from_velocity(
+  const double right_traction_wheel_vel, const double left_traction_wheel_vel,
+  const double steer_pos, const double dt)
+{
+  return sk_impl_.update_from_velocity(
+    right_traction_wheel_vel, left_traction_wheel_vel, steer_pos, dt);
+}
+
+bool SteeringOdometry::update_from_velocity(
+  const double right_traction_wheel_vel, const double left_traction_wheel_vel,
+  const double right_steer_pos, const double left_steer_pos, const double dt)
+{
+  return sk_impl_.update_from_velocity(
+    right_traction_wheel_vel, left_traction_wheel_vel, right_steer_pos, left_steer_pos, dt);
+}
+
+void SteeringOdometry::update_open_loop(const double v_bx, const double omega_bz, const double dt)
+{
+  sk_impl_.update_open_loop(v_bx, omega_bz, dt);
+}
+
+void SteeringOdometry::set_wheel_params(double wheel_radius, double wheel_base, double wheel_track)
+{
+  sk_impl_.set_wheel_params(wheel_radius, wheel_base, wheel_track);
+}
+
+void SteeringOdometry::set_wheel_params(
+  double wheel_radius, double wheel_base, double wheel_track_steering, double wheel_track_traction)
+{
+  sk_impl_.set_wheel_params(wheel_radius, wheel_base, wheel_track_steering, wheel_track_traction);
+}
+
+void SteeringOdometry::set_velocity_rolling_window_size(size_t velocity_rolling_window_size)
+{
+  sk_impl_.set_velocity_rolling_window_size(velocity_rolling_window_size);
+}
+
+void SteeringOdometry::set_odometry_type(const unsigned int type)
+{
+  sk_impl_.set_odometry_type(type);
+}
+
+std::tuple<std::vector<double>, std::vector<double>> SteeringOdometry::get_commands(
+  const double v_bx, const double omega_bz, const bool open_loop,
+  const bool reduce_wheel_speed_until_steering_reached)
+{
+  return sk_impl_.get_commands(
+    v_bx, omega_bz, open_loop, reduce_wheel_speed_until_steering_reached);
+}
+
+void SteeringOdometry::reset_odometry() { sk_impl_.reset_odometry(); }
+
+}  // namespace steering_odometry
