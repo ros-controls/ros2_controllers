fc82d3b71f7256892af877a01e312b7f6cb5e435
pastoriomarco <pastoriomarco@gmail.com>
2026-01-15 10:12:23 +0100

    revert get_lifecycle_id() to get_lifecycle_state().id()

--- joint_trajectory_controller/src/joint_trajectory_controller.cpp
+++ joint_trajectory_controller/src/joint_trajectory_controller.cpp
@@ -715,7 +715,7 @@ void JointTrajectoryController::query_state_service(
 {
   const auto logger = get_node()->get_logger();
   // Preconditions
-  if (get_lifecycle_id() != lifecycle_msgs::msg::State::PRIMARY_STATE_ACTIVE)
+  if (get_lifecycle_state().id() != lifecycle_msgs::msg::State::PRIMARY_STATE_ACTIVE)
   {
     RCLCPP_ERROR(logger, "Can't sample trajectory. Controller is not active.");
     response->success = false;
@@ -1337,7 +1337,7 @@ void JointTrajectoryController::publish_state(
     {
       state_msg_.output = command_current_;
     }
-    state_publisher_->msg_.speed_scaling_factor = scaling_factor_;
+    state_msg_.speed_scaling_factor = scaling_factor_.load();
 
     state_publisher_->try_publish(state_msg_);
   }
@@ -1365,7 +1365,7 @@ rclcpp_action::GoalResponse JointTrajectoryController::goal_received_callback(
   RCLCPP_INFO(get_node()->get_logger(), "Received new action goal");
 
   // Precondition: Running controller
-  if (get_lifecycle_id() == lifecycle_msgs::msg::State::PRIMARY_STATE_INACTIVE)
+  if (get_lifecycle_state().id() == lifecycle_msgs::msg::State::PRIMARY_STATE_INACTIVE)
   {
     RCLCPP_ERROR(
       get_node()->get_logger(), "Can't accept new action goals. Controller is not running.");
