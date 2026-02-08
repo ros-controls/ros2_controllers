a9e90b1d322cf11385783af5a29fd546c9d2acd3
Ishan1923 <ecdev4ishan@gmail.com>
2026-02-09 00:54:19 +0530

    fix: use unordered_set for logging and remove unsafe to_string call

--- gpio_controllers/src/gpio_command_controller.cpp
+++ gpio_controllers/src/gpio_command_controller.cpp
@@ -441,10 +441,15 @@ void GpioCommandController::apply_state_value(
     }
     else
     {
-      RCLCPP_DEBUG_ONCE(
-        get_node()->get_logger(),
-        "Interface '%s' has unsupported type '%s'. Only 'double' and 'bool' are supported.",
-        interface_name.c_str(), type.to_string().c_str());
+      if (nan_interfaces_warned_.find(interface_name) == nan_interfaces_warned_.end())
+      {
+        RCLCPP_INFO(
+          get_node()->get_logger(),
+          "Interface '%s' has unsupported type. Only 'double' and 'bool' are supported.",
+          interface_name.c_str());
+
+        nan_interfaces_warned_.insert(interface_name);
+      }
     }
 
     state_msg.interface_values[gpio_index].values[interface_index] = state_value;
