656ba7fc23002593d4af1876af83ab301db39701
Christoph Fröhlich <christophfroehlich@users.noreply.github.com>
2026-03-04 00:10:54 +0100

    Consistently add <cmath> include with define for windows (#2193)
    (cherry picked from commit 8b90d97fd006532db5e7caeb3eeb2e059fbac5e2)
    
    # Conflicts:
    #	admittance_controller/test/test_admittance_rule_impl.cpp
    #	imu_sensor_broadcaster/test/test_imu_transform.cpp

--- joint_trajectory_controller/test/test_trajectory_controller.cpp
+++ joint_trajectory_controller/test/test_trajectory_controller.cpp
@@ -12,6 +12,10 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+#ifndef _USE_MATH_DEFINES
+#define _USE_MATH_DEFINES
+#endif
+
 #include <cstddef>
 
 #include <chrono>
