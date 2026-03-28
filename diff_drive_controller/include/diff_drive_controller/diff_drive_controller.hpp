c4764763d3043880e69acdabcff089541b406fb0
Bhavin Umatiya <umatiyabhavin@gmail.com>
2026-03-28 15:54:54 +0530

    Remove publish_rate functionality for consistency
    This change follows maintainer feedback on issue #2035 to remove the publish_rate feature instead of just fixing its initialization, for architectural alignment across controllers.

--- diff_drive_controller/include/diff_drive_controller/diff_drive_controller.hpp
+++ diff_drive_controller/include/diff_drive_controller/diff_drive_controller.hpp
@@ -149,10 +149,6 @@ protected:
 
   rclcpp::Time previous_update_timestamp_{0};
 
-  // publish rate limiter
-  double publish_rate_ = 50.0;
-  rclcpp::Duration publish_period_ = rclcpp::Duration::from_nanoseconds(0);
-  rclcpp::Time previous_publish_timestamp_{0};
 
   rclcpp::Service<control_msgs::srv::SetOdometry>::SharedPtr set_odom_service_;
   std::atomic<bool> set_odom_requested_{false};
