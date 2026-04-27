4f00329031507d4e7dad4cd2d958ce6098ea17c7
greencookie-afk <mithunchakladar123@gmail.com>
2026-04-27 14:51:32 +0530

    refactor(joint_state_broadcaster): use warn expression macro

--- joint_state_broadcaster/src/joint_state_broadcaster.cpp
+++ joint_state_broadcaster/src/joint_state_broadcaster.cpp
@@ -125,14 +125,11 @@ controller_interface::CallbackReturn JointStateBroadcaster::on_configure(
     {
       map_interface_to_joint_state_[interface] = interface;
       // Warn if custom mapping is being ignored
-      if (interface != interface_to_map)
-      {
-        RCLCPP_WARN(
-          get_node()->get_logger(),
-          "Mapping from '%s' to interface '%s' will not be done, because '%s' is defined "
-          "in 'interface' parameter.",
-          interface_to_map.c_str(), interface.c_str(), interface.c_str());
-      }
+      RCLCPP_WARN_EXPRESSION(
+        get_node()->get_logger(), interface != interface_to_map,
+        "Mapping from '%s' to interface '%s' will not be done, because '%s' is defined "
+        "in 'interface' parameter.",
+        interface_to_map.c_str(), interface.c_str(), interface.c_str());
     }
     else
     {
