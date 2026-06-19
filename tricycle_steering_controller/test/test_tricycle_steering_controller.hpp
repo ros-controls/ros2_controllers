58969bf363185e941da1f6077ad07e9b15079d84
Junius Santoso <9374081+jsantoso91@users.noreply.github.com>
2026-06-19 02:11:13 -0500

    Final test cleanup - call appropriate lifecycle transitions (#2429)

--- tricycle_steering_controller/test/test_tricycle_steering_controller.hpp
+++ tricycle_steering_controller/test/test_tricycle_steering_controller.hpp
@@ -54,11 +54,8 @@ using controller_interface::deactivate_succeeds;
 
 namespace
 {
-constexpr auto NODE_SUCCESS = controller_interface::CallbackReturn::SUCCESS;
-constexpr auto NODE_ERROR = controller_interface::CallbackReturn::ERROR;
 const double COMMON_THRESHOLD = 1e-6;
 }  // namespace
-// namespace
 
 // subclassing and friending so we can access member variables
 class TestableTricycleSteeringController
