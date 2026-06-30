5d4d0987fa390d1969397430383d05e552e0cfe0
Souri Rishik <vsouri2705@gmail.com>
2026-06-30 23:40:23 +0530

    fix: correct test_load_controller tests for motion_primitives and pid_controller (#2445)
    (cherry picked from commit f6bcf6ccda02048e021d51655323bb8f6e2452e9)
    
    # Conflicts:
    #	effort_controllers/test/test_load_joint_group_effort_controller.cpp
    #	motion_primitives_controllers/CMakeLists.txt

--- pid_controller/test/test_load_pid_controller.cpp
+++ pid_controller/test/test_load_pid_controller.cpp
@@ -37,7 +37,11 @@ TEST(TestLoadPidController, load_controller)
       ros2_control_test_assets::minimal_robot_urdf),
     executor, "test_controller_manager");
 
-  ASSERT_NO_THROW(cm.load_controller("test_pid_controller", "pid_controller/PidController"));
+  const std::string test_file_path =
+    std::string(TEST_FILES_DIRECTORY) + "/pid_controller_params.yaml";
+  cm.set_parameter({"test_pid_controller.params_file", test_file_path});
+  cm.set_parameter({"test_pid_controller.type", "pid_controller/PidController"});
+  ASSERT_NE(cm.load_controller("test_pid_controller"), nullptr);
 
   rclcpp::shutdown();
 }
