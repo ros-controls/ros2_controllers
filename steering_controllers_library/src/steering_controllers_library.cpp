99aaf9c601fd750929d0359cd10c4e50a4cdccbd
Christoph Fröhlich <christophfroehlich@users.noreply.github.com>
2026-07-05 22:30:00 +0200

    Fix merge conflict

--- steering_controllers_library/src/steering_controllers_library.cpp
+++ steering_controllers_library/src/steering_controllers_library.cpp
@@ -626,15 +626,11 @@ controller_interface::return_type SteeringControllersLibrary::update_and_write_c
   {
     for (size_t i = 0; i < params_.traction_joints_names.size(); i++)
     {
-<<<<<<< HEAD
-      command_interfaces_[i].set_value(0.0);
-=======
       if (!command_interfaces_[i].set_value(0.0, std::numeric_limits<unsigned int>::max()))
       {
         RCLCPP_WARN(logger, "Unable to set command interface to value 0.0");
         return controller_interface::return_type::OK;
       }
->>>>>>> c7b97ca (Fix safety concerns with halt logic across controllers (#2326))
     }
   }
 
