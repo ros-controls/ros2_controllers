28e4169d83c7c6692c4cf88cea1607e900639fa4
Christoph Froehlich <christoph.froehlich@ait.ac.at>
2026-08-04 11:54:28 +0000

    Remove unused variable assignments

--- steering_controllers_library/test/test_steering_controllers_library.hpp
+++ steering_controllers_library/test/test_steering_controllers_library.hpp
@@ -88,7 +88,8 @@ public:
     const rclcpp_lifecycle::State & previous_state) override
   {
     // export_reference_interfaces() populates ordered_exported_reference_interfaces_
-    auto ref_itfs = export_reference_interfaces();
+    export_reference_interfaces();
+    export_state_interfaces();
     return steering_controllers_library::SteeringControllersLibrary::on_activate(previous_state);
   }
 
