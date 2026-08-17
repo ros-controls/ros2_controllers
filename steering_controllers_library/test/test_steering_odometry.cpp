8c0beae98e3376ce04d36922020d39a11b546416
Dylan Gallagher <76841713+Dylan-Gallagher@users.noreply.github.com>
2026-08-17 21:18:54 +0100

    Add closed-form steering integration tests (#2567)
    (cherry picked from commit 74ff2bbd7e3df43ce33612a757a1d0e3269cc037)

--- steering_controllers_library/test/test_steering_odometry.cpp
+++ steering_controllers_library/test/test_steering_odometry.cpp
@@ -1,4 +1,5 @@
 // Copyright (c) 2023, Virtual Vehicle Research GmbH
+// Copyright (c) 2026, Dylan Gallagher
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -16,8 +17,109 @@
 
 #include <gmock/gmock.h>
 #include <cmath>
+#include <string>
 
 #include "steering_controllers_library/steering_kinematics.hpp"
+
+namespace
+{
+constexpr double TOLERANCE = 1e-10;
+
+struct SteeringConfiguration
+{
+  unsigned int type;
+  const char * name;
+};
+
+struct Pose
+{
+  double x;
+  double y;
+  double heading;
+};
+
+Pose integrate_twist(
+  const Pose & initial, const double linear, const double angular, const double dt)
+{
+  if (steering_kinematics::is_close_to_zero(angular * dt))
+  {
+    const double middle_heading = initial.heading + angular * dt * 0.5;
+    return {
+      initial.x + linear * std::cos(middle_heading) * dt,
+      initial.y + linear * std::sin(middle_heading) * dt, initial.heading + angular * dt};
+  }
+
+  const double final_heading = initial.heading + angular * dt;
+  const double turning_radius = linear / angular;
+  return {
+    initial.x + turning_radius * (std::sin(final_heading) - std::sin(initial.heading)),
+    initial.y - turning_radius * (std::cos(final_heading) - std::cos(initial.heading)),
+    final_heading};
+}
+
+auto PoseNear(const Pose & expected)
+{
+  return ::testing::AllOf(
+    ::testing::Field("x", &Pose::x, ::testing::DoubleNear(expected.x, TOLERANCE)),
+    ::testing::Field("y", &Pose::y, ::testing::DoubleNear(expected.y, TOLERANCE)),
+    ::testing::Field(
+      "heading", &Pose::heading, ::testing::DoubleNear(expected.heading, TOLERANCE)));
+}
+
+class SteeringOdometryIntegratorTest : public ::testing::TestWithParam<SteeringConfiguration>
+{
+protected:
+  void expect_open_loop(
+    const Pose & initial, const double linear, const double angular, const double dt) const
+  {
+    SCOPED_TRACE(
+      ::testing::Message() << "initial=" << initial.x << "," << initial.y << "," << initial.heading
+                           << " twist=" << linear << "," << angular << " dt=" << dt);
+    steering_kinematics::SteeringKinematics odom(1);
+    odom.set_odometry_type(GetParam().type);
+    odom.set_odometry(initial.x, initial.y, initial.heading);
+    odom.update_open_loop(linear, angular, dt);
+
+    EXPECT_THAT(
+      (Pose{odom.get_x(), odom.get_y(), odom.get_heading()}),
+      PoseNear(integrate_twist(initial, linear, angular, dt)));
+    EXPECT_DOUBLE_EQ(odom.get_linear(), linear);
+    EXPECT_DOUBLE_EQ(odom.get_angular(), angular);
+  }
+};
+
+std::string configuration_name(const ::testing::TestParamInfo<SteeringConfiguration> & information)
+{
+  return information.param.name;
+}
+
+// cppcheck-suppress syntaxError
+// Cppcheck does not expand the GoogleTest parameterized-test macro.
+TEST_P(SteeringOdometryIntegratorTest, integrates_open_loop_straight)
+{
+  // Retain the former Ackermann-only zero-pose case and extend it to every configuration.
+  expect_open_loop(Pose{0.0, 0.0, 0.0}, 2.0, 0.0, 0.5);
+  expect_open_loop(Pose{-1.2, 0.7, 0.4}, 1.1, 0.0, 0.35);
+}
+
+TEST_P(SteeringOdometryIntegratorTest, integrates_open_loop_arcs)
+{
+  for (const double direction : {-1.0, 1.0})
+  {
+    // Retain both former Ackermann-only turn directions with a stronger exact-pose oracle.
+    expect_open_loop(Pose{0.0, 0.0, 0.0}, 1.0, direction, 1.0);
+    expect_open_loop(Pose{0.8, -0.45, -0.3}, 1.3, direction * 0.45, 0.6);
+  }
+}
+
+INSTANTIATE_TEST_SUITE_P(
+  SteeringConfigurations, SteeringOdometryIntegratorTest,
+  ::testing::Values(
+    SteeringConfiguration{steering_kinematics::BICYCLE_CONFIG, "Bicycle"},
+    SteeringConfiguration{steering_kinematics::TRICYCLE_CONFIG, "Tricycle"},
+    SteeringConfiguration{steering_kinematics::ACKERMANN_CONFIG, "Ackermann"}),
+  configuration_name);
+}  // namespace
 
 TEST(TestSteeringOdometry, initialize)
 {
@@ -43,42 +145,6 @@ TEST(TestSteeringOdometry, ackermann_odometry)
   EXPECT_NEAR(odom.get_angular(), .1, 1e-3);
   EXPECT_NEAR(odom.get_x(), .1, 1e-3);
   EXPECT_NEAR(odom.get_heading(), .01, 1e-3);
-}
-
-TEST(TestSteeringOdometry, ackermann_odometry_openloop_linear)
-{
-  steering_kinematics::SteeringKinematics odom(1);
-  odom.set_wheel_params(1., 2., 1.);
-  odom.set_odometry_type(steering_kinematics::ACKERMANN_CONFIG);
-  odom.update_open_loop(2., 0., 0.5);
-  EXPECT_DOUBLE_EQ(odom.get_linear(), 2.);
-  EXPECT_DOUBLE_EQ(odom.get_x(), 1.);
-  EXPECT_DOUBLE_EQ(odom.get_y(), 0.);
-}
-
-TEST(TestSteeringOdometry, ackermann_odometry_openloop_angular_left)
-{
-  steering_kinematics::SteeringKinematics odom(1);
-  odom.set_wheel_params(1., 2., 1.);
-  odom.set_odometry_type(steering_kinematics::ACKERMANN_CONFIG);
-  odom.update_open_loop(1., 1., 1.);
-  EXPECT_DOUBLE_EQ(odom.get_linear(), 1.);
-  EXPECT_DOUBLE_EQ(odom.get_angular(), 1.);
-
-  EXPECT_GT(odom.get_x(), 0);  // pos x
-  EXPECT_GT(odom.get_y(), 0);  // pos y, ie. left
-}
-
-TEST(TestSteeringOdometry, ackermann_odometry_openloop_angular_right)
-{
-  steering_kinematics::SteeringKinematics odom(1);
-  odom.set_wheel_params(1., 2., 1.);
-  odom.set_odometry_type(steering_kinematics::ACKERMANN_CONFIG);
-  odom.update_open_loop(1., -1., 1.);
-  EXPECT_DOUBLE_EQ(odom.get_linear(), 1.);
-  EXPECT_DOUBLE_EQ(odom.get_angular(), -1.);
-  EXPECT_GT(odom.get_x(), 0);  // pos x
-  EXPECT_LT(odom.get_y(), 0);  // neg y ie. right
 }
 
 TEST(TestSteeringOdometry, ackermann_IK_linear)
